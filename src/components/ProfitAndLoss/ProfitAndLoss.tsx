9b911a69c98c0dc1f9c88c74a63c4579374ce0f6
Jon Yurek <jyurek@gmail.com>
2023-11-28 10:27:06 -0500

    Create useWindowSize to trigger size repaints
    The ProfitAndLoss Chart only takes absolute number sizes for its width, so if
    we want to be able to fit the chart to the width of the ProfitAndLoss
    component (or allow for percent/relative sizes in the future), we need to be
    able to measure its containing element. But since the size of its containing
    element may change when the window is resized, we can use window-size-events
    to be able to trigger our `useEffect`s or `useLayoutEffect`s. This is a handy
    way of wrapping that in a hook. The original code was found on StackOverflow
    and since it wasn't worth rewriting, it was copied and a credit link was put
    into a comment.

--- src/components/ProfitAndLoss/ProfitAndLoss.tsx
+++ src/components/ProfitAndLoss/ProfitAndLoss.tsx
@@ -1,13 +1,18 @@
 import React, { PropsWithChildren, createContext } from 'react'
 import { useProfitAndLoss } from '../../hooks/useProfitAndLoss'
+import { useWindowSize } from '../../hooks/useWindowSize/useWindowSize'
 import { ProfitAndLossDatePicker } from '../ProfitAndLossDatePicker'
 import { ProfitAndLossTable } from '../ProfitAndLossTable'
 import { endOfMonth, startOfMonth } from 'date-fns'
 
-const PNLContext = createContext<ReturnType<typeof useProfitAndLoss>>({
+type PNLContextType = ReturnType<typeof useProfitAndLoss> & {
+  dimensions?: DOMRect
+}
+const PNLContext = createContext<PNLContextType>({
   data: undefined,
   isLoading: true,
   error: undefined,
+  dimensions: undefined,
   dateRange: {
     startDate: startOfMonth(new Date()),
     endDate: endOfMonth(new Date()),
@@ -17,9 +22,19 @@ const PNLContext = createContext<ReturnType<typeof useProfitAndLoss>>({
 
 const ProfitAndLoss = ({ children }: PropsWithChildren) => {
   const contextData = useProfitAndLoss()
+  const size = useWindowSize()
+  const boundsRef = useRef<HTMLDivElement>(null)
+  const [dimensions, setDimensions] = useState<DOMRect | undefined>(undefined)
+  useLayoutEffect(() => {
+    if (boundsRef.current) {
+      setDimensions(boundsRef.current.getBoundingClientRect())
+    }
+  }, size)
   return (
-    <PNLContext.Provider value={contextData}>
-      <div className="Layer__profit-and-loss">{children}</div>
+    <PNLContext.Provider value={{ ...contextData, dimensions }}>
+      <div ref={boundsRef} className="Layer__profit-and-loss">
+        {children}
+      </div>
     </PNLContext.Provider>
   )
 }
