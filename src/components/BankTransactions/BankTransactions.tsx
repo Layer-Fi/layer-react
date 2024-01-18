7eb6654ba716af672ed9b3999afa348bf4473682
tomaszantas <tom@altalogy.com>
2024-01-18 20:29:18 +0100

    idea: table sekeleton loader

--- src/components/BankTransactions/BankTransactions.tsx
+++ src/components/BankTransactions/BankTransactions.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useBankTransactions } from '../../hooks/useBankTransactions'
 import { BankTransaction, CategorizationStatus } from '../../types'
 import { BankTransactionRow } from '../BankTransactionRow'
@@ -37,8 +37,11 @@ const filterVisibility =
   }
 
 export const BankTransactions = () => {
+  const componentRef = useRef<HTMLDivElement>(null)
+  const [skeletonRows, setSkeletonRows] = useState(0)
+
   const [display, setDisplay] = useState<DisplayState>(DisplayState.review)
-  const { data } = useBankTransactions()
+  const { data, isLoading } = useBankTransactions()
   const bankTransactions = data.filter(filterVisibility(display))
   const onCategorizationDisplayChange = (
     event: React.ChangeEvent<HTMLInputElement>,
@@ -51,8 +54,30 @@ export const BankTransactions = () => {
   const [openRows, setOpenRows] = useState<Record<string, boolean>>({})
   const toggleOpen = (id: string) =>
     setOpenRows({ ...openRows, [id]: !openRows[id] })
+
+  useEffect(() => {
+    if (componentRef.current) {
+      const rows = Math.floor(componentRef.current.offsetHeight / 70) - 2
+      console.log(componentRef.current.offsetHeight, rows)
+      setSkeletonRows(rows)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (componentRef.current && isLoading) {
+      const rows = Math.floor(componentRef.current.offsetHeight / 70) - 2
+      console.log(componentRef.current.offsetHeight, rows)
+      setSkeletonRows(rows)
+    }
+  }, [isLoading])
+
+  console.log('bankTransactions', bankTransactions)
+
   return (
-    <div className='Layer__component Layer__bank-transactions'>
+    <div
+      className='Layer__component Layer__bank-transactions'
+      ref={componentRef}
+    >
       <header className='Layer__bank-transactions__header'>
         <h2 className='Layer__bank-transactions__title'>Transactions</h2>
         <RadioButtonGroup
@@ -86,17 +111,42 @@ export const BankTransactions = () => {
             Actions
           </div>
         </div>
-        {bankTransactions.map((bankTransaction: BankTransaction) => (
-          <BankTransactionRow
-            key={bankTransaction.id}
-            dateFormat={dateFormat}
-            bankTransaction={bankTransaction}
-            isOpen={openRows[bankTransaction.id]}
-            toggleOpen={toggleOpen}
-            editable={display === DisplayState.review}
-          />
-        ))}
+
+        {!isLoading && bankTransactions?.length > 0
+          ? bankTransactions.map((bankTransaction: BankTransaction) => (
+              <BankTransactionRow
+                key={bankTransaction.id}
+                dateFormat={dateFormat}
+                bankTransaction={bankTransaction}
+                isOpen={openRows[bankTransaction.id]}
+                toggleOpen={toggleOpen}
+                editable={display === DisplayState.review}
+              />
+            ))
+          : null}
       </div>
+
+      {isLoading || !bankTransactions || bankTransactions?.length === 0 ? (
+        <div
+          style={{
+            width: '100%',
+            height: '100%',
+            overflow: 'hidden',
+            maxHeight: '100%',
+            position: 'relative',
+          }}
+        >
+          {[...Array(skeletonRows)].map((i, idx) => (
+            <div key={idx} style={{ padding: 25, background: '#fff' }}>
+              <div
+                className='Layer__table-row__skeleton'
+                style={{ width: '100%' }}
+              />
+            </div>
+          ))}
+          <div className='Layer__table-row__skeleton-overflow' />
+        </div>
+      ) : null}
     </div>
   )
 }
