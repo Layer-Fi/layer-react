2f2d97f2caf5105306da28cc826098268088e0e0
tomaszantas <tom@altalogy.com>
2024-04-03 15:08:09 +0200

    fix: change stroke-line into strokeline

--- src/icons/ArrowRightCircle.tsx
+++ src/icons/ArrowRightCircle.tsx
@@ -13,20 +13,20 @@ const ArrowRightCircle = ({ size = 18, ...props }: IconSvgProps) => (
     <path
       d='M9 16.5C13.1421 16.5 16.5 13.1421 16.5 9C16.5 4.85786 13.1421 1.5 9 1.5C4.85786 1.5 1.5 4.85786 1.5 9C1.5 13.1421 4.85786 16.5 9 16.5Z'
       stroke='currentColor'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     />
     <path
       d='M9 12L12 9L9 6'
       stroke='currentColor'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     />
     <path
       d='M6 9H12'
       stroke='currentColor'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     />
   </svg>
 )
