448146a20a52d4feee11a5d060c30c5c9b993eee
Arkadiusz Celer <arku@altalogy.com>
2024-04-17 13:57:32 +0200

    Add badges states

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -9,12 +9,21 @@ export enum BadgeSize {
   MEDIUM = 'medium',
 }
 
+export enum BadgeVariants {
+  DEFAULT = 'default',
+  SUCCESS = 'success',
+  WARNING = 'warning',
+  ERROR = 'error',
+}
+
 export interface BadgeProps {
   children: ReactNode
   icon?: ReactNode
   onClick?: ButtonProps['onClick']
   tooltip?: ReactNode
   size?: BadgeSize
+  variants?: BadgeVariants
+  hoverable?: boolean
 }
 
 export const Badge = ({
@@ -23,12 +32,16 @@ export const Badge = ({
   children,
   tooltip,
   size = BadgeSize.MEDIUM,
+  variants = BadgeVariants.DEFAULT,
+  hoverable = false,
 }: BadgeProps) => {
   const baseProps = {
     className: classNames(
       'Layer__badge',
+      hoverable && !tooltip ? 'Layer__badge--with-hover' : '',
       onClick || tooltip ? 'Layer__badge--clickable' : '',
       `Layer__badge--${size}`,
+      `Layer__badge--${variants}`,
     ),
     onClick,
     children,
