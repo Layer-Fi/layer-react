cb90ff92bf905d872e3002df6fc17adceceaba10
Tom <tom@altalogy.com>
2024-05-16 13:12:18 +0200

    feat: improved rwd (#137)

--- src/views/AccountingOverview/AccountingOverview.tsx
+++ src/views/AccountingOverview/AccountingOverview.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Header } from '../../components/Container'
 import { Onboarding } from '../../components/Onboarding'
 import { ProfitAndLoss } from '../../components/ProfitAndLoss'
+import { Toggle } from '../../components/Toggle'
 import { TransactionToReviewCard } from '../../components/TransactionToReviewCard'
 import { Heading, HeadingSize } from '../../components/Typography'
 import { View } from '../../components/View'
+import classNames from 'classnames'
 
 export interface AccountingOverviewProps {
   title?: string
@@ -12,11 +14,15 @@ export interface AccountingOverviewProps {
   onTransactionsToReviewClick?: () => void
 }
 
+type PnlToggleOption = 'revenue' | 'expenses'
+
 export const AccountingOverview = ({
   title = 'Accounting overview',
   enableOnboarding = false,
   onTransactionsToReviewClick,
 }: AccountingOverviewProps) => {
+  const [pnlToggle, setPnlToggle] = useState<PnlToggleOption>('revenue')
+
   return (
     <ProfitAndLoss asContainer={false}>
       <View title={title} headerControls={<ProfitAndLoss.DatePicker />}>
@@ -36,10 +42,37 @@ export const AccountingOverview = ({
           <ProfitAndLoss.Chart />
         </Container>
         <div className='accounting-overview-profit-and-loss-charts'>
-          <Container name='accounting-overview-profit-and-loss-chart'>
+          <Toggle
+            name='pnl-detailed-charts'
+            options={[
+              {
+                value: 'revenue',
+                label: 'Revenue',
+              },
+              {
+                value: 'expenses',
+                label: 'Expenses',
+              },
+            ]}
+            selected={pnlToggle}
+            onChange={e => setPnlToggle(e.target.value as PnlToggleOption)}
+          />
+          <Container
+            name={classNames(
+              'accounting-overview-profit-and-loss-chart',
+              pnlToggle !== 'revenue' &&
+                'accounting-overview-profit-and-loss-chart--hidden',
+            )}
+          >
             <ProfitAndLoss.DetailedCharts scope='revenue' hideClose={true} />
           </Container>
-          <Container name='accounting-overview-profit-and-loss-chart'>
+          <Container
+            name={classNames(
+              'accounting-overview-profit-and-loss-chart',
+              pnlToggle !== 'expenses' &&
+                'accounting-overview-profit-and-loss-chart--hidden',
+            )}
+          >
             <ProfitAndLoss.DetailedCharts scope='expenses' hideClose={true} />
           </Container>
         </div>
