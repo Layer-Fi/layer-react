15b3a05615a36c46b3a861a40acd37cd96d1da50
Arkadiusz Celer <32065451+ArkuVonSymfon@users.noreply.github.com>
2024-07-30 16:28:35 +0200

    feat: use pnl summaries data in transactions to review and in (#245)

--- src/contexts/BankTransactionsContext/BankTransactionsContext.tsx
+++ src/contexts/BankTransactionsContext/BankTransactionsContext.tsx
@@ -23,7 +23,7 @@ export const BankTransactionsContext =
     activate: () => undefined,
     display: DisplayState.review,
     fetchMore: () => {},
-    hasMore: false
+    hasMore: false,
   })
 
 export const useBankTransactionsContext = () =>
