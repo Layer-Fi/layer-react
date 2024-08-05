d08197e91e5b559e8b3c473d669deb48040f2f24
Daniel O'Neel <danieloneel@gmail.com>
2024-08-05 14:16:37 -0700

    allow specifying allowed reports

--- src/views/Reports/Reports.tsx
+++ src/views/Reports/Reports.tsx
@@ -1,19 +1,19 @@
 import React, { RefObject, useContext, useRef, useState } from 'react'
 import { Layer } from '../../api/layer'
 import { BalanceSheet } from '../../components/BalanceSheet'
+import { BalanceSheetStringOverrides } from '../../components/BalanceSheet/BalanceSheet'
 import { Button, ButtonVariant, RetryButton } from '../../components/Button'
 import { Container } from '../../components/Container'
 import { Panel } from '../../components/Panel'
 import { ProfitAndLoss } from '../../components/ProfitAndLoss'
+import { ProfitAndLossDetailedChartsStringOverrides } from '../../components/ProfitAndLossDetailedCharts/ProfitAndLossDetailedCharts'
+import { ProfitAndLossTableStringOverrides } from '../../components/ProfitAndLossTable/ProfitAndLossTable'
 import { StatementOfCashFlow } from '../../components/StatementOfCashFlow'
+import { StatementOfCashFlowStringOverrides } from '../../components/StatementOfCashFlow/StatementOfCashFlow'
 import { Toggle } from '../../components/Toggle'
 import { View } from '../../components/View'
 import { useLayerContext } from '../../contexts/LayerContext'
 import DownloadCloud from '../../icons/DownloadCloud'
-import { BalanceSheetStringOverrides } from '../../components/BalanceSheet/BalanceSheet'
-import { StatementOfCashFlowStringOverrides } from '../../components/StatementOfCashFlow/StatementOfCashFlow'
-import { ProfitAndLossDetailedChartsStringOverrides } from '../../components/ProfitAndLossDetailedCharts/ProfitAndLossDetailedCharts'
-import { ProfitAndLossTableStringOverrides } from '../../components/ProfitAndLossTable/ProfitAndLossTable'
 
 interface ReportsStringOverrides {
   title?: string
@@ -29,6 +29,7 @@ interface ReportsStringOverrides {
 export interface ReportsProps {
   title?: string // deprecated
   stringOverrides?: ReportsStringOverrides
+  enabledReports?: ReportType[]
 }
 
 type ReportType = 'profitAndLoss' | 'balanceSheet' | 'statementOfCashFlow'
@@ -44,7 +45,11 @@ interface DownloadButtonStringOverrides {
   retryButtonText?: string
 }
 
-const DownloadButton = ({ stringOverrides} : { stringOverrides?: DownloadButtonStringOverrides }) => {
+const DownloadButton = ({
+  stringOverrides,
+}: {
+  stringOverrides?: DownloadButtonStringOverrides
+}) => {
   const { dateRange } = useContext(ProfitAndLoss.Context)
   const { auth, businessId, apiUrl } = useLayerContext()
   const [requestFailed, setRequestFailed] = useState(false)
@@ -82,7 +87,7 @@ const DownloadButton = ({ stringOverrides} : { stringOverrides?: DownloadButtonS
       className='Layer__download-retry-btn'
       error={'Approval failed. Check connection and retry in few seconds.'}
     >
-      {stringOverrides?.retryButtonText || "Retry"}
+      {stringOverrides?.retryButtonText || 'Retry'}
     </RetryButton>
   ) : (
     <Button
@@ -90,44 +95,78 @@ const DownloadButton = ({ stringOverrides} : { stringOverrides?: DownloadButtonS
       rightIcon={<DownloadCloud size={12} />}
       onClick={handleClick}
     >
-      {stringOverrides?.downloadButtonText || "Download"}
+      {stringOverrides?.downloadButtonText || 'Download'}
     </Button>
   )
 }
 
-export const Reports = ({ title, stringOverrides }: ReportsProps) => {
+const getOptions = (enabledReports: ReportType[]) => {
+  return [
+    enabledReports.includes('profitAndLoss')
+      ? {
+          value: 'profitAndLoss',
+          label: 'Profit & Loss',
+        }
+      : null,
+    enabledReports.includes('balanceSheet')
+      ? {
+          value: 'balanceSheet',
+          label: 'Balance Sheet',
+        }
+      : null,
+    enabledReports.includes('statementOfCashFlow')
+      ? {
+          value: 'statementOfCashFlow',
+          label: 'Statement of Cash Flow',
+        }
+      : null,
+  ].filter(o => !!o) as ReportOption[]
+}
+
+export const Reports = ({
+  title,
+  stringOverrides,
+  enabledReports = ['profitAndLoss', 'balanceSheet', 'statementOfCashFlow'],
+}: ReportsProps) => {
   const containerRef = useRef<HTMLDivElement>(null)
-  const [activeTab, setActiveTab] = useState<ReportType>('profitAndLoss')
+  const [activeTab, setActiveTab] = useState<ReportType>(enabledReports[0])
+
+  const options = getOptions(enabledReports)
+  const defaultTitle =
+    enabledReports.length > 1
+      ? 'Reports'
+      : options.find(option => (option.value = enabledReports[0]))?.label
 
   return (
-    <View title={stringOverrides?.title || title || "Reports"}>
-      <div className='Layer__component Layer__header__actions'>
-        <Toggle
-          name='reports-tabs'
-          options={
-            [
-              { value: 'profitAndLoss', label: 'Profit & loss' },
-              { value: 'balanceSheet', label: 'Balance sheet' },
-              {
-                value: 'statementOfCashFlow',
-                label: 'Statement of Cash Flow',
-              },
-            ] as ReportOption[]
-          }
-          selected={activeTab}
-          onChange={opt => setActiveTab(opt.target.value as ReportType)}
-        />
-      </div>
+    <View title={stringOverrides?.title || title || defaultTitle}>
+      {enabledReports.length > 1 && (
+        <div className='Layer__component Layer__header__actions'>
+          <Toggle
+            name='reports-tabs'
+            options={options}
+            selected={activeTab}
+            onChange={opt => setActiveTab(opt.target.value as ReportType)}
+          />
+        </div>
+      )}
       <Container name='reports' ref={containerRef}>
         <ProfitAndLoss asContainer={false}>
-          <ReportsPanel containerRef={containerRef} openReport={activeTab} stringOverrides={stringOverrides} />
+          <ReportsPanel
+            containerRef={containerRef}
+            openReport={activeTab}
+            stringOverrides={stringOverrides}
+          />
         </ProfitAndLoss>
       </Container>
     </View>
   )
 }
 
-const ReportsPanel = ({ containerRef, openReport, stringOverrides }: ReportsPanelProps) => {
+const ReportsPanel = ({
+  containerRef,
+  openReport,
+  stringOverrides,
+}: ReportsPanelProps) => {
   const { sidebarScope } = useContext(ProfitAndLoss.Context)
   return (
     <>
@@ -137,7 +176,9 @@ const ReportsPanel = ({ containerRef, openReport, stringOverrides }: ReportsPane
           headerControls={
             <>
               <ProfitAndLoss.DatePicker />
-              <DownloadButton stringOverrides={stringOverrides?.downloadButton}/>
+              <DownloadButton
+                stringOverrides={stringOverrides?.downloadButton}
+              />
             </>
           }
         >
@@ -158,8 +199,14 @@ const ReportsPanel = ({ containerRef, openReport, stringOverrides }: ReportsPane
           </Panel>
         </View>
       )}
-      {openReport === 'balanceSheet' && <BalanceSheet stringOverrides={stringOverrides?.balanceSheet}/>}
-      {openReport === 'statementOfCashFlow' && <StatementOfCashFlow stringOverrides={stringOverrides?.statementOfCashflow}/>}
+      {openReport === 'balanceSheet' && (
+        <BalanceSheet stringOverrides={stringOverrides?.balanceSheet} />
+      )}
+      {openReport === 'statementOfCashFlow' && (
+        <StatementOfCashFlow
+          stringOverrides={stringOverrides?.statementOfCashflow}
+        />
+      )}
     </>
   )
 }
