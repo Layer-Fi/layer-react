0513ca2b5359542227f6c8094df83f072e2c22ad
Daniel O'Neel <danieloneel@gmail.com>
2024-08-27 00:32:38 -0400

    introduce multiselect input

---
+++ src/components/Input/MultiSelect.tsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import ReactSelect, {
+  DropdownIndicatorProps,
+  GroupBase,
+  MultiValue,
+  OptionsOrGroups,
+  components,
+} from 'react-select'
+import ChevronDownFill from '../../icons/ChevronDownFill'
+import { Tooltip, TooltipContent, TooltipTrigger } from '../Tooltip'
+import classNames from 'classnames'
+
+export interface SelectProps<T> {
+  name?: string
+  options?: OptionsOrGroups<T, GroupBase<T>>
+  className?: string
+  classNamePrefix?: string
+  value?: T
+  onChange: (selected: MultiValue<T>) => void
+  disabled?: boolean
+  placeholder?: string
+  isInvalid?: boolean
+  errorMessage?: string
+}
+
+const DropdownIndicator:
+  | React.ComponentType<DropdownIndicatorProps<any, true, GroupBase<any>>>
+  | null
+  | undefined = props => {
+  return (
+    <components.DropdownIndicator {...props}>
+      <ChevronDownFill />
+    </components.DropdownIndicator>
+  )
+}
+
+export const MultiSelect = <T,>({
+  name,
+  options,
+  className,
+  classNamePrefix = 'Layer__select',
+  value,
+  onChange,
+  disabled,
+  placeholder,
+  isInvalid,
+  errorMessage,
+}: SelectProps<T>) => {
+  const baseClassName = classNames(
+    'Layer__select',
+    isInvalid ? 'Layer__select--error' : '',
+    className,
+  )
+  return (
+    <Tooltip disabled={!isInvalid || !errorMessage}>
+      <TooltipTrigger className='Layer__input-tooltip'>
+        <ReactSelect<T, true>
+          name={name}
+          className={baseClassName}
+          classNamePrefix={classNamePrefix}
+          placeholder={placeholder ?? 'Select...'}
+          options={options}
+          value={value}
+          onChange={newValue => newValue && onChange(newValue)}
+          menuPortalTarget={document.body}
+          styles={{ menuPortal: base => ({ ...base, zIndex: 9999 }) }}
+          components={{ DropdownIndicator }}
+          isDisabled={disabled}
+          isMulti={true}
+        />
+      </TooltipTrigger>
+      <TooltipContent className='Layer__tooltip'>{errorMessage}</TooltipContent>
+    </Tooltip>
+  )
+}
