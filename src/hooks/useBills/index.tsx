5232534bbdeb84229dadda7e79d0b43231f4aa31
Arkadiusz Celer <arku@altalogy.com>
2024-10-03 12:54:57 +0200

    add bills view

---
+++ src/hooks/useBills/index.tsx
@@ -0,0 +1,1 @@
+export { useBills } from './useBills'
---
+++ src/views/Bills/Bills.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import { Bills } from '../../components/Bills'
+import { Header } from '../../components/Container'
+import { DatePicker } from '../../components/DatePicker'
+import { HeaderRow, HeaderCol } from '../../components/Header'
+import { View } from '../../components/View'
+
+export interface BillsStringOverrides {
+  title?: string
+  paidToggleOption?: string
+  unpaidToggleOption?: string
+}
+
+export interface ChartOfAccountsOptions {
+  templateAccountsEditable?: boolean
+}
+
+export interface BillsProps {
+  showTitle?: boolean
+  stringOverrides?: BillsStringOverrides
+}
+
+export const BillsView = ({
+  showTitle = true,
+  stringOverrides,
+}: BillsProps) => {
+  const [activeTab, setActiveTab] = useState('unpaid')
+
+  return (
+    <View
+      title={stringOverrides?.title || 'Bills'}
+      showHeader={showTitle}
+      viewClassName='Layer__bills__view'
+      header={
+        <Header>
+          <HeaderRow>
+            <HeaderCol>
+              <DatePicker
+                mode='monthRangePicker'
+                selected={new Date()}
+                onChange={() => {}}
+              />
+            </HeaderCol>
+          </HeaderRow>
+        </Header>
+      }
+    >
+      <Bills activeTab={activeTab} setActiveTab={setActiveTab} />
+    </View>
+  )
+}
