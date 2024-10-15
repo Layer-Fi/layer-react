15944084ee85d00526c1406a56f63a925d0f450c
Tom Antas <tom@altalogy.com>
2024-10-15 15:48:36 +0200

    fix: debit before credit in journal

--- src/components/JournalForm/JournalFormEntryLines.tsx
+++ src/components/JournalForm/JournalFormEntryLines.tsx
@@ -44,113 +44,115 @@ export const JournalFormEntryLines = ({
 
   return (
     <>
-      {Object.keys(Direction).map((direction, idx) => {
-        return (
-          <div
-            key={'Layer__journal__form__input-group-' + idx}
-            className='Layer__journal__form__input-group Layer__journal__form__input-group__border'
-          >
-            <Text
-              className='Layer__journal__form__input-group__title'
-              size={TextSize.lg}
+      {Object.keys(Direction)
+        .reverse()
+        .map((direction, idx) => {
+          return (
+            <div
+              key={'Layer__journal__form__input-group-' + idx}
+              className='Layer__journal__form__input-group Layer__journal__form__input-group__border'
             >
-              Add {humanizeEnum(direction)} Account
-            </Text>
-            {entrylineItems?.map((item, idx) => {
-              if (item.direction !== direction) {
-                return null
-              }
-              return (
-                <div
-                  className='Layer__journal__form__input-group__line-item'
-                  key={direction + '-' + idx}
-                >
-                  <InputGroup name={direction} label='Amount' inline={true}>
-                    <InputWithBadge
-                      name={direction}
-                      placeholder='$0.00'
-                      value={convertNumberToCurrency(item.amount)}
-                      disabled={sendingForm}
-                      badge={humanizeEnum(direction)}
-                      variant={
-                        item.direction === 'CREDIT'
-                          ? BadgeVariant.SUCCESS
-                          : BadgeVariant.WARNING
-                      }
-                      onChange={e =>
-                        changeFormData(
-                          'amount',
-                          convertCurrencyToNumber(
-                            (e.target as HTMLInputElement).value,
-                          ),
-                          idx,
-                        )
-                      }
-                      isInvalid={Boolean(
-                        form?.errors?.lineItems.find(
-                          x => x.id === idx && x.field === 'amount',
-                        ),
-                      )}
-                      errorMessage={
-                        form?.errors?.lineItems.find(
-                          x => x.id === idx && x.field === 'amount',
-                        )?.message
-                      }
-                    />
-                  </InputGroup>
-                  <InputGroup
-                    name='account-name'
-                    label='Account name'
-                    inline={true}
-                  >
-                    <Select
-                      options={parentOptions}
-                      value={{
-                        value: item.account_identifier.id,
-                        label: item.account_identifier.name,
-                      }}
-                      onChange={sel =>
-                        changeFormData(
-                          'parent',
-                          sel,
-                          idx,
-                          accountsData?.accounts,
-                        )
-                      }
-                      isInvalid={Boolean(
-                        form?.errors?.lineItems.find(
-                          x => x.id === idx && x.field === 'account',
-                        ),
-                      )}
-                      errorMessage={
-                        form?.errors?.lineItems.find(
-                          x => x.id === idx && x.field === 'account',
-                        )?.message
-                      }
-                    />
-                    {idx >= 2 && (
-                      <IconButton
-                        className='Layer__remove__button'
-                        onClick={() => removeEntryLine(idx)}
-                        icon={<Trash />}
-                      />
-                    )}
-                  </InputGroup>
-                </div>
-              )
-            })}
-            {(config.form.addEntryLinesLimit === undefined ||
-              config.form.addEntryLinesLimit > entrylineItems?.length) && (
-              <TextButton
-                className='Layer__journal__add-entry-line'
-                onClick={() => addEntryLine(direction as Direction)}
+              <Text
+                className='Layer__journal__form__input-group__title'
+                size={TextSize.lg}
               >
-                Add next account
-              </TextButton>
-            )}
-          </div>
-        )
-      })}
+                Add {humanizeEnum(direction)} Account
+              </Text>
+              {entrylineItems?.map((item, idx) => {
+                if (item.direction !== direction) {
+                  return null
+                }
+                return (
+                  <div
+                    className='Layer__journal__form__input-group__line-item'
+                    key={direction + '-' + idx}
+                  >
+                    <InputGroup name={direction} label='Amount' inline={true}>
+                      <InputWithBadge
+                        name={direction}
+                        placeholder='$0.00'
+                        value={convertNumberToCurrency(item.amount)}
+                        disabled={sendingForm}
+                        badge={humanizeEnum(direction)}
+                        variant={
+                          item.direction === 'CREDIT'
+                            ? BadgeVariant.SUCCESS
+                            : BadgeVariant.WARNING
+                        }
+                        onChange={e =>
+                          changeFormData(
+                            'amount',
+                            convertCurrencyToNumber(
+                              (e.target as HTMLInputElement).value,
+                            ),
+                            idx,
+                          )
+                        }
+                        isInvalid={Boolean(
+                          form?.errors?.lineItems.find(
+                            x => x.id === idx && x.field === 'amount',
+                          ),
+                        )}
+                        errorMessage={
+                          form?.errors?.lineItems.find(
+                            x => x.id === idx && x.field === 'amount',
+                          )?.message
+                        }
+                      />
+                    </InputGroup>
+                    <InputGroup
+                      name='account-name'
+                      label='Account name'
+                      inline={true}
+                    >
+                      <Select
+                        options={parentOptions}
+                        value={{
+                          value: item.account_identifier.id,
+                          label: item.account_identifier.name,
+                        }}
+                        onChange={sel =>
+                          changeFormData(
+                            'parent',
+                            sel,
+                            idx,
+                            accountsData?.accounts,
+                          )
+                        }
+                        isInvalid={Boolean(
+                          form?.errors?.lineItems.find(
+                            x => x.id === idx && x.field === 'account',
+                          ),
+                        )}
+                        errorMessage={
+                          form?.errors?.lineItems.find(
+                            x => x.id === idx && x.field === 'account',
+                          )?.message
+                        }
+                      />
+                      {idx >= 2 && (
+                        <IconButton
+                          className='Layer__remove__button'
+                          onClick={() => removeEntryLine(idx)}
+                          icon={<Trash />}
+                        />
+                      )}
+                    </InputGroup>
+                  </div>
+                )
+              })}
+              {(config.form.addEntryLinesLimit === undefined ||
+                config.form.addEntryLinesLimit > entrylineItems?.length) && (
+                <TextButton
+                  className='Layer__journal__add-entry-line'
+                  onClick={() => addEntryLine(direction as Direction)}
+                >
+                  Add next account
+                </TextButton>
+              )}
+            </div>
+          )
+        })}
     </>
   )
 }
