903b7346ca228f0827684ad4752cbd9d3a4c03cb
Kesin Ryan Dehejia <kesin@layerfi.com>
2024-10-17 15:51:24 -0700

    Adding filtering to PNL Report

--- src/views/ProjectProfitability/ProjectProfitability.tsx
+++ src/views/ProjectProfitability/ProjectProfitability.tsx
@@ -1,25 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, type RefObject } from 'react'
 import { BankTransactions } from '../../components/BankTransactions'
 import { Container } from '../../components/Container'
-import { DateRangeDatePickerModes } from '../../components/DatePicker/DatePicker'
 import { ProfitAndLoss } from '../../components/ProfitAndLoss'
 import { ProjectSelector } from '../../components/ProjectProfitability/ProjectSelector'
 import { Toggle } from '../../components/Toggle'
 import { View } from '../../components/View'
-import {
-  TagsStoreProvider,
-  useTagsStore,
-} from '../../contexts/Tags/TagsStoreProvider'
+import { TagsStoreProvider } from '../../contexts/Tags/TagsStoreProvider'
+import { useTags } from '../../hooks/tags/useTags'
 import { useElementViewSize } from '../../hooks/useElementViewSize'
-import { DisplayState, MoneyFormat } from '../../types'
+import { DisplayState } from '../../types'
 import { View as ViewType } from '../../types/general'
+import type { ReportsStringOverrides } from '../Reports/Reports'
 import classNames from 'classnames'
 
 function BankTransactionsWithProjectFilters() {
-  const { activeCategory, activeKey } = useTagsStore()
+  const { tag, category } = useTags()
   const tagFilter =
-    activeKey && activeCategory
-      ? { tagKey: activeKey, tagValues: [activeCategory] }
+    tag && category
+      ? { tagKey: tag.value, tagValues: [category.value] }
       : 'None'
 
   return (
@@ -30,6 +28,97 @@ function BankTransactionsWithProjectFilters() {
         tagFilter,
       }}
     />
+  )
+}
+
+function ProfitAndLossWithProjectFilters({
+  activeTab,
+  stringOverrides,
+}: {
+  activeTab: ProjectTab
+  stringOverrides?: ReportsStringOverrides
+}) {
+  const [view, setView] = useState<ViewBreakpoint>('desktop')
+  const [pnlToggle, setPnlToggle] = useState<PnlToggleOption>('expenses')
+  const containerRef = useElementViewSize<HTMLDivElement>(newView =>
+    setView(newView),
+  )
+
+  const { tag, category } = useTags()
+  const tagFilter =
+    tag && category ? { key: tag.value, values: [category.value] } : undefined
+
+  const profitAndLossConfig = {
+    datePickerMode: 'monthPicker',
+    csvMoneyFormat: 'DOLLAR_STRING',
+  } as const
+
+  return (
+    <ProfitAndLoss asContainer={false} tagFilter={tagFilter}>
+      <Container name='project' ref={containerRef}>
+        <>
+          {activeTab === 'overview' && (
+            <div className='Layer__accounting-overview-profit-and-loss-charts'>
+              <Toggle
+                name='pnl-detailed-charts'
+                options={[
+                  {
+                    value: 'revenue',
+                    label: 'Revenue',
+                  },
+                  {
+                    value: 'expenses',
+                    label: 'Expenses',
+                  },
+                ]}
+                selected={pnlToggle}
+                onChange={e => setPnlToggle(e.target.value as PnlToggleOption)}
+              />
+              <Container
+                name={classNames(
+                  'accounting-overview-profit-and-loss-chart',
+                  pnlToggle !== 'revenue' &&
+                    'accounting-overview-profit-and-loss-chart--hidden',
+                )}
+              >
+                <ProfitAndLoss.DetailedCharts
+                  scope='revenue'
+                  hideClose={true}
+                  // stringOverrides={stringOverrides?.profitAndLoss?.detailedCharts}
+                  // chartColorsList={chartColorsList}
+                />
+              </Container>
+              <Container
+                name={classNames(
+                  'accounting-overview-profit-and-loss-chart',
+                  pnlToggle !== 'expenses' &&
+                    'accounting-overview-profit-and-loss-chart--hidden',
+                )}
+              >
+                <ProfitAndLoss.DetailedCharts
+                  scope='expenses'
+                  hideClose={true}
+                  // stringOverrides={stringOverrides?.profitAndLoss?.detailedCharts}
+                  // chartColorsList={chartColorsList}
+                />
+              </Container>
+            </div>
+          )}
+          {activeTab === 'transactions' && (
+            <BankTransactionsWithProjectFilters />
+          )}
+          {activeTab === 'report' && (
+            <ProfitAndLoss.Report
+              stringOverrides={stringOverrides}
+              datePickerMode={profitAndLossConfig.datePickerMode}
+              csvMoneyFormat={profitAndLossConfig.csvMoneyFormat}
+              parentRef={containerRef}
+              view={view}
+            />
+          )}
+        </>
+      </Container>
+    </ProfitAndLoss>
   )
 }
 
@@ -52,20 +141,6 @@ export const ProjectProfitabilityView = ({
   stringOverrides,
 }: ProjectProfitabilityProps) => {
   const [activeTab, setActiveTab] = useState<ProjectTab>('overview')
-  const [view, setView] = useState<ViewBreakpoint>('desktop')
-  const [pnlToggle, setPnlToggle] = useState<PnlToggleOption>('expenses')
-  const containerRef = useElementViewSize<HTMLDivElement>(newView =>
-    setView(newView),
-  )
-
-  const comparisonConfig = undefined
-  const profitAndLossConfig: {
-    datePickerMode?: DateRangeDatePickerModes
-    csvMoneyFormat?: MoneyFormat
-  } = {
-    datePickerMode: 'monthPicker',
-    csvMoneyFormat: 'DOLLAR_STRING',
-  }
 
   return (
     <TagsStoreProvider initialActiveKey='project'>
@@ -92,74 +167,7 @@ export const ProjectProfitabilityView = ({
             onChange={opt => setActiveTab(opt.target.value as ProjectTab)}
           />
         </div>
-        <ProfitAndLoss asContainer={false}>
-          <Container name='project' ref={containerRef}>
-            <>
-              {activeTab === 'overview' && (
-                <div className='Layer__accounting-overview-profit-and-loss-charts'>
-                  <Toggle
-                    name='pnl-detailed-charts'
-                    options={[
-                      {
-                        value: 'revenue',
-                        label: 'Revenue',
-                      },
-                      {
-                        value: 'expenses',
-                        label: 'Expenses',
-                      },
-                    ]}
-                    selected={pnlToggle}
-                    onChange={e =>
-                      setPnlToggle(e.target.value as PnlToggleOption)
-                    }
-                  />
-                  <Container
-                    name={classNames(
-                      'accounting-overview-profit-and-loss-chart',
-                      pnlToggle !== 'revenue' &&
-                        'accounting-overview-profit-and-loss-chart--hidden',
-                    )}
-                  >
-                    <ProfitAndLoss.DetailedCharts
-                      scope='revenue'
-                      hideClose={true}
-                      // stringOverrides={stringOverrides?.profitAndLoss?.detailedCharts}
-                      // chartColorsList={chartColorsList}
-                    />
-                  </Container>
-                  <Container
-                    name={classNames(
-                      'accounting-overview-profit-and-loss-chart',
-                      pnlToggle !== 'expenses' &&
-                        'accounting-overview-profit-and-loss-chart--hidden',
-                    )}
-                  >
-                    <ProfitAndLoss.DetailedCharts
-                      scope='expenses'
-                      hideClose={true}
-                      // stringOverrides={stringOverrides?.profitAndLoss?.detailedCharts}
-                      // chartColorsList={chartColorsList}
-                    />
-                  </Container>
-                </div>
-              )}
-              {activeTab === 'transactions' && (
-                <BankTransactionsWithProjectFilters />
-              )}
-              {activeTab === 'report' && (
-                <ProfitAndLoss.Report
-                  stringOverrides={stringOverrides}
-                  comparisonConfig={comparisonConfig}
-                  datePickerMode={profitAndLossConfig?.datePickerMode}
-                  csvMoneyFormat={profitAndLossConfig?.csvMoneyFormat}
-                  parentRef={containerRef}
-                  view={view}
-                />
-              )}
-            </>
-          </Container>
-        </ProfitAndLoss>
+        <ProfitAndLossWithProjectFilters activeTab={activeTab} />
       </View>
     </TagsStoreProvider>
   )
