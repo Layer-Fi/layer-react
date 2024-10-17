b753dd219c19d70ca1b65594e0b27e5f2c2f8c19
Andrew Acomb <acombandrew@gmail.com>
2024-10-17 10:37:38 -0700

    toggle

--- src/views/ProjectProfitability/ProjectProfitability.tsx
+++ src/views/ProjectProfitability/ProjectProfitability.tsx
@@ -1,46 +1,58 @@
 import React, { useState } from 'react'
-import { ChartOfAccounts } from '../../components/ChartOfAccounts'
 import { Container } from '../../components/Container'
-import { DateRangeDatePickerModes } from '../../components/DatePicker/DatePicker'
-import { Journal } from '../../components/Journal'
-import { ProfitAndLoss } from '../../components/ProfitAndLoss'
-import { ProfitAndLossCompareOptionsProps } from '../../components/ProfitAndLossCompareOptions/ProfitAndLossCompareOptions'
 import { Toggle } from '../../components/Toggle'
 import { View } from '../../components/View'
 import { useElementViewSize } from '../../hooks/useElementViewSize'
-import { MoneyFormat } from '../../types'
 import { View as ViewType } from '../../types/general'
-import { ReportsStringOverrides } from '../Reports/Reports'
 
 type ViewBreakpoint = ViewType | undefined
 
+export interface ProjectsStringOverrides {
+  title?: string
+}
+
 export interface ProjectProfitabilityProps {
   showTitle?: boolean
-  stringOverrides?: ReportsStringOverrides
+  stringOverrides?: ProjectsStringOverrides
 }
 
 export const ProjectProfitabilityView = ({
   showTitle,
   stringOverrides,
 }: ProjectProfitabilityProps) => {
-  const [activeTab, setActiveTab] = useState<string>('')
+  const [activeTab, setActiveTab] = useState<ProjectTab>('overview')
   const [view, setView] = useState<ViewBreakpoint>('desktop')
 
   const containerRef = useElementViewSize<HTMLDivElement>(newView =>
     setView(newView),
   )
 
+  type ProjectTab = 'overview' | 'transactions' | 'reports'
+
   return (
     <View title={stringOverrides?.title || ''} showHeader={showTitle}>
       <div className='Layer__component Layer__header__actions'>
         <Toggle
-          name='project-profitability-tabs'
-          options={[]}
+          name='project-tabs'
+          options={[
+            {
+              value: 'overview',
+              label: 'Overview',
+            },
+            {
+              value: 'transactions',
+              label: 'Transactions',
+            },
+            {
+              value: 'reports',
+              label: 'Reports',
+            }
+          ]}
           selected={activeTab}
-          onChange={opt => setActiveTab('')}
+          onChange={opt => setActiveTab(opt.target.value as ProjectTab)}
         />
       </div>
-      <Container name='project profitability' ref={containerRef}>
+      <Container name='project' ref={containerRef}>
         <>Empty container</>
       </Container>
     </View>
