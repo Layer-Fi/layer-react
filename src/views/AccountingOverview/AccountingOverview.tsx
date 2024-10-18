1cc8179eb9a7a3809eb26c7ba718fd758b711a16
Tom Antas <tom@altalogy.com>
2024-10-18 13:34:32 +0200

    clean overview

--- src/views/AccountingOverview/AccountingOverview.tsx
+++ src/views/AccountingOverview/AccountingOverview.tsx
@@ -2,15 +2,14 @@ import React, { ReactNode, useState } from 'react'
 import { Container } from '../../components/Container'
 import { Header, HeaderCol, HeaderRow } from '../../components/Header'
 import { Onboarding } from '../../components/Onboarding'
-import { LinkAccounts } from '../../components/PlatformOnboarding'
 import { ProfitAndLoss } from '../../components/ProfitAndLoss'
 import { ProfitAndLossDetailedChartsStringOverrides } from '../../components/ProfitAndLossDetailedCharts/ProfitAndLossDetailedCharts'
 import { ProfitAndLossSummariesStringOverrides } from '../../components/ProfitAndLossSummaries/ProfitAndLossSummaries'
 import { Toggle } from '../../components/Toggle'
 import { TransactionToReviewCard } from '../../components/TransactionToReviewCard'
 import { View } from '../../components/View'
-import { OnboardingStep } from '../../types/layer_context'
 import classNames from 'classnames'
+import { OnboardingStep } from '../../types/layer_context'
 
 interface AccountingOverviewStringOverrides {
   header?: string
