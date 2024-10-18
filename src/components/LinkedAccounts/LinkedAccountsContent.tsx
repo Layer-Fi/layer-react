d77a69dded6401cd26bafac51dba735b93ddf0e9
Tom Antas <tom@altalogy.com>
2024-10-18 13:33:23 +0200

    add account button

--- src/components/LinkedAccounts/LinkedAccountsContent.tsx
+++ src/components/LinkedAccounts/LinkedAccountsContent.tsx
@@ -1,6 +1,9 @@
 import React, { useContext } from 'react'
 import { LinkedAccountsContext } from '../../contexts/LinkedAccountsContext'
+import PlusIcon from '../../icons/PlusIcon'
+import { Text, TextSize } from '../Typography'
 import { LinkedAccountItemThumb } from './LinkedAccountItemThumb'
+import classNames from 'classnames'
 
 interface LinkedAccountsDataProps {
   asWidget?: boolean
@@ -15,7 +18,15 @@ export const LinkedAccountsContent = ({
   showUnlinkItem,
   showBreakConnection,
 }: LinkedAccountsDataProps) => {
-  const { data } = useContext(LinkedAccountsContext)
+  const { data, addConnection } = useContext(LinkedAccountsContext)
+
+  const linkedAccountsNewAccountClassName = classNames(
+    'Layer__linked-accounts__new-account',
+    asWidget && '--as-widget',
+    showLedgerBalance && '--show-ledger-balance',
+    showUnlinkItem && '--show-unlink-item',
+    showBreakConnection && '--show-break-connection',
+  )
 
   return (
     <div className='Layer__linked-accounts__list'>
@@ -29,6 +40,20 @@ export const LinkedAccountsContent = ({
           asWidget={asWidget}
         />
       ))}
+      <div
+        role='button'
+        tabIndex={0}
+        aria-label='new-account'
+        onClick={() => addConnection('PLAID')}
+        className={linkedAccountsNewAccountClassName}
+      >
+        <div className='Layer__linked-accounts__new-account-label'>
+          <PlusIcon size={15} />
+          <Text as='span' size={'sm' as TextSize}>
+            Add Account
+          </Text>
+        </div>
+      </div>
     </div>
   )
 }
