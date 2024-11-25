598d52be9f9623d56ff320aea690187450a2c083
bryceAebi <bryce.a.aebi@gmail.com>
2024-11-25 22:14:58 +0900

    Multidoc upload support

--- src/components/TasksListItem/TasksListItem.tsx
+++ src/components/TasksListItem/TasksListItem.tsx
@@ -1,61 +1,61 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { TasksContext } from '../../contexts/TasksContext'
-import AlertCircle from '../../icons/AlertCircle'
-import Check from '../../icons/Check'
-import ChevronDownFill from '../../icons/ChevronDownFill'
-import { isComplete, TaskTypes } from '../../types/tasks'
-import { Button, ButtonVariant } from '../Button'
-import { FileInput } from '../Input'
-import { Textarea } from '../Textarea'
-import { Text, TextSize } from '../Typography'
-import classNames from 'classnames'
+import React, { useContext, useEffect, useState } from "react";
+import { TasksContext } from "../../contexts/TasksContext";
+import AlertCircle from "../../icons/AlertCircle";
+import Check from "../../icons/Check";
+import ChevronDownFill from "../../icons/ChevronDownFill";
+import { isComplete, TaskTypes } from "../../types/tasks";
+import { Button, ButtonVariant } from "../Button";
+import { FileInput } from "../Input";
+import { Textarea } from "../Textarea";
+import { Text, TextSize } from "../Typography";
+import classNames from "classnames";
 
 export const TasksListItem = ({
   task,
   goToNextPageIfAllComplete,
   defaultOpen,
 }: {
-  task: TaskTypes
-  goToNextPageIfAllComplete: (task: TaskTypes) => void
-  defaultOpen: boolean
+  task: TaskTypes;
+  goToNextPageIfAllComplete: (task: TaskTypes) => void;
+  defaultOpen: boolean;
 }) => {
-  const [isOpen, setIsOpen] = useState(defaultOpen)
-  const [userResponse, setUserResponse] = useState(task.user_response || '')
+  const [isOpen, setIsOpen] = useState(defaultOpen);
+  const [userResponse, setUserResponse] = useState(task.user_response || "");
 
   const { submitResponseToTask, uploadDocumentForTask } =
-    useContext(TasksContext)
+    useContext(TasksContext);
 
   const taskBodyClassName = classNames(
-    'Layer__tasks-list-item__body',
-    isOpen && 'Layer__tasks-list-item__body--expanded',
-    isComplete(task.status) && 'Layer__tasks-list-item--completed',
-  )
+    "Layer__tasks-list-item__body",
+    isOpen && "Layer__tasks-list-item__body--expanded",
+    isComplete(task.status) && "Layer__tasks-list-item--completed"
+  );
 
   const taskHeadClassName = classNames(
-    'Layer__tasks-list-item__head-info',
+    "Layer__tasks-list-item__head-info",
     isComplete(task.status)
-      ? 'Layer__tasks-list-item--completed'
-      : 'Layer__tasks-list-item--pending',
-  )
+      ? "Layer__tasks-list-item--completed"
+      : "Layer__tasks-list-item--pending"
+  );
 
   const taskItemClassName = classNames(
-    'Layer__tasks-list-item',
-    isOpen && 'Layer__tasks-list-item__expanded',
-  )
+    "Layer__tasks-list-item",
+    isOpen && "Layer__tasks-list-item__expanded"
+  );
 
   useEffect(() => {
-    setIsOpen(defaultOpen)
-  }, [defaultOpen])
+    setIsOpen(defaultOpen);
+  }, [defaultOpen]);
 
   return (
-    <div className='Layer__tasks-list-item-wrapper'>
+    <div className="Layer__tasks-list-item-wrapper">
       <div className={taskItemClassName}>
         <div
-          className='Layer__tasks-list-item__head'
+          className="Layer__tasks-list-item__head"
           onClick={() => setIsOpen(!isOpen)}
         >
           <div className={taskHeadClassName}>
-            <div className='Layer__tasks-list-item__head-info__status'>
+            <div className="Layer__tasks-list-item__head-info__status">
               {isComplete(task.status) ? (
                 <Check size={12} />
               ) : (
@@ -66,16 +66,16 @@ export const TasksListItem = ({
           </div>
           <ChevronDownFill
             size={16}
-            className='Layer__tasks__expand-icon'
+            className="Layer__tasks__expand-icon"
             style={{
-              transform: isOpen ? 'rotate(0deg)' : 'rotate(-180deg)',
+              transform: isOpen ? "rotate(0deg)" : "rotate(-180deg)",
             }}
           />
         </div>
         <div className={taskBodyClassName}>
-          <div className='Layer__tasks-list-item__body-info'>
+          <div className="Layer__tasks-list-item__body-info">
             <Text size={TextSize.sm}>{task.question}</Text>
-            {task.user_response_type === 'FREE_RESPONSE' ? (
+            {task.user_response_type === "FREE_RESPONSE" ? (
               <Textarea
                 value={userResponse}
                 onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
@@ -83,15 +83,15 @@ export const TasksListItem = ({
                 }
               />
             ) : null}
-            <div className='Layer__tasks-list-item__actions'>
-              {task.user_response_type === 'UPLOAD_DOCUMENT' ? (
+            <div className="Layer__tasks-list-item__actions">
+              {task.user_response_type === "UPLOAD_DOCUMENT" ? (
                 <FileInput
                   onUpload={(file: File) => {
-                    uploadDocumentForTask(task.id, file)
-                    setIsOpen(false)
-                    goToNextPageIfAllComplete(task)
+                    uploadDocumentForTask(task.id, file);
+                    setIsOpen(false);
+                    goToNextPageIfAllComplete(task);
                   }}
-                  text='Upload file'
+                  text="Upload file(s)"
                   disabled={
                     task.completed_at != null ||
                     task.user_marked_completed_at != null ||
@@ -106,14 +106,14 @@ export const TasksListItem = ({
                   }
                   variant={ButtonVariant.secondary}
                   onClick={() => {
-                    submitResponseToTask(task.id, userResponse)
-                    setIsOpen(false)
-                    goToNextPageIfAllComplete(task)
+                    submitResponseToTask(task.id, userResponse);
+                    setIsOpen(false);
+                    goToNextPageIfAllComplete(task);
                   }}
                 >
                   {userResponse && userResponse.length === 0
-                    ? 'Update'
-                    : 'Save'}
+                    ? "Update"
+                    : "Save"}
                 </Button>
               )}
             </div>
@@ -121,5 +121,5 @@ export const TasksListItem = ({
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
