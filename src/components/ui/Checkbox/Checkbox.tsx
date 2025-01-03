6b15d2c9ee932bf73b0c880bbb1466d0e97f075b
Tom Antas <tom@altalogy.com>
2025-01-03 17:01:23 +0100

    fix checkbox

--- src/components/ui/Checkbox/Checkbox.tsx
+++ src/components/ui/Checkbox/Checkbox.tsx
@@ -1,20 +1,22 @@
 import { Check } from 'lucide-react'
-import React from 'react'
-import { Checkbox as ReactAriaCheckbox, type CheckboxProps} from 'react-aria-components'
+import React, { ReactNode } from 'react'
+import { Checkbox as ReactAriaCheckbox, type CheckboxProps as AriaCheckboxProps } from 'react-aria-components'
 
 const CLASS_NAME = 'Layer__Checkbox'
 
-export function Checkbox({children, ...props}: Omit<CheckboxProps, 'className'>) {
+export type CheckboxProps = Omit<AriaCheckboxProps, 'className' | 'children'> & { children?: ReactNode | ReactNode[] }
+
+export function Checkbox({ children, ...props }: CheckboxProps) {
   return (
     <ReactAriaCheckbox {...props} className={CLASS_NAME}>
-      {() =>
+      {() => (
         <>
           <div slot='checkbox'>
             <Check />
           </div>
           {children}
         </>
-      }
+      )}
     </ReactAriaCheckbox>
   )
 }
