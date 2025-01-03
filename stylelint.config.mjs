c1ca7dc7ace2999e13e2a1f9e396d328e0f10fe6
Tom Antas <tom@altalogy.com>
2025-01-03 15:27:19 +0100

    pr review

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,20 +1,11 @@
+/** @type {import('stylelint').Config} */
 export default {
-  // Define the files to lint
-  files: ['src/**/*.scss', 'src/**/*.css'],
-
   plugins: ['@stylistic/stylelint-plugin'],
-
-  // Extend the standard and SCSS configurations
   extends: ['stylelint-config-standard', 'stylelint-config-standard-scss'],
-
-  // Add custom rules
   rules: {
-    // Class names
     'selector-class-pattern': [
       '^(\\.?(Layer__)?(--)?[a-z]([a-z0-9-]+)?(__([a-z0-9]+-?)+)*(--([a-z0-9]+-?)+){0,2})$',
     ],
-
-    // Disallow unknown at-rules
     'at-rule-no-unknown': [
       true,
       {
@@ -30,26 +21,13 @@ export default {
         ],
       },
     ],
-
-    // Disallow units for zero values
     'length-zero-no-unit': true,
-
-    // Require a specific naming pattern for custom properties
     'custom-property-pattern': '^[a-z0-9-]+$',
-
-    // Prohibit duplicate selectors
     'no-duplicate-selectors': true,
-
-    // Indentation
     '@stylistic/indentation': 2,
-
     '@stylistic/max-empty-lines': 1,
-
     '@stylistic/no-eol-whitespace': true,
-
     '@stylistic/no-missing-end-of-source-newline': true,
-
-    // Require a space after colon in declarations
     '@stylistic/declaration-colon-space-after': 'always',
   },
 }
