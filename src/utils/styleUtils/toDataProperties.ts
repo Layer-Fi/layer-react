23ed6a598e5fc41595196b35254ca4dacef429ea
Kesin Ryan Dehejia <34279289+imkesin@users.noreply.github.com>
2025-01-13 08:06:52 -0800

    fix: `toDataProperties` needs greater typesafety to warn callers (#468)
    * chore: fix console error for showreceiptuploadcolumn
    
    * fix: `toDataProperties` needs greater typesafety to warn callers
    
    ---------
    
    Co-authored-by: Tom Antas <tom@altalogy.com>

--- src/utils/styleUtils/toDataProperties.ts
+++ src/utils/styleUtils/toDataProperties.ts
@@ -1,26 +1,29 @@
 type AllowedDataValue = string | number | true
 
-type DataPrefix<T extends string> = `data-${T}`
+type DataPrefix<T extends Lowercase<string>> = `data-${T}`
 
 type DataProperties<T> = {
   [K in keyof T as DataPrefix<
-    Extract<K, string>
+    Extract<K, Lowercase<string>>
   >]: T[K] extends AllowedDataValue ? T[K] : never
 }
 
-export function toDataProperties<T extends Record<string, unknown>>(input: T) {
+export function toDataProperties<
+  TKeys extends Lowercase<string>,
+  TValues,
+>(input: Record<TKeys, TValues>) {
   const unsafeProperties = Object.fromEntries(
     Object.entries(input)
       .map(([key, value]) => {
         if (
-          typeof value !== 'string' &&
-          typeof value !== 'number' &&
-          value !== true
+          typeof value !== 'string'
+          && typeof value !== 'number'
+          && value !== true
         ) {
           return null
         }
 
-        return [`data-${key}`, value] as const
+        return [`data-${key as Lowercase<string>}`, value] as const
       })
       .filter((entry): entry is Exclude<typeof entry, null> => entry !== null),
   )
@@ -29,5 +32,5 @@ export function toDataProperties<T extends Record<string, unknown>>(input: T) {
    * The `fromEntries` signature does not preserve the type of the keys, so we
    * need an explicit cast.
    */
-  return unsafeProperties as DataProperties<T>
+  return unsafeProperties as DataProperties<Record<TKeys, Extract<TValues, AllowedDataValue>>>
 }
