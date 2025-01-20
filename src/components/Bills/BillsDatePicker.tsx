37b0058aca19189805301aa214349be31aa13b26
Tom Antas <tom@altalogy.com>
2025-01-20 14:45:02 +0100

    update details

---
+++ src/components/Bills/BillsDatePicker.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { DatePicker } from '../DatePicker/DatePicker'
+import { useBillsContext } from '../../contexts/BillsContext'
+
+export const BillsDatePicker = () => {
+  const { dateRange, setDateRange } = useBillsContext()
+
+  return (
+    <DatePicker
+      displayMode='monthRangePicker'
+      selected={[dateRange.startDate, dateRange.endDate]}
+      onChange={(e) => {
+        if (Array.isArray(e) && e[0] && e[1]) {
+          setDateRange({
+            startDate: e[0],
+            endDate: e[1],
+          })
+        }
+      }}
+      wrapperClassName='Layer__bills__main-datepicker'
+    />
+  )
+}
