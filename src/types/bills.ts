10aca9679e1ddf56f1d24cdaaea605ae2d2feb77
Tom Antas <tom@altalogy.com>
2025-01-23 20:53:00 +0100

    update bill types

--- src/types/bills.ts
+++ src/types/bills.ts
@@ -8,81 +8,81 @@ export type BillStatusFilter = 'PAID' | 'UNPAID'
 export const UnpaidStatuses = ['SENT', 'PARTIALLY_PAID']
 export const PaidStatuses = ['PAID']
 
-export const BillTerms = [{
-  id: 'due_on_receipt',
+type BillTerm = 'DUE_ON_RECEIPT' | 'NET_10' | 'NET_15' | 'NET_30' | 'NET_60'
+
+export const BillTerms: {
+  id: BillTerm
+  label: string
+}[] = [{
+  id: 'DUE_ON_RECEIPT',
   label: 'Due on receipt',
 }, {
-  id: 'net_10',
+  id: 'NET_10',
   label: 'Net 10',
 }, {
-  id: 'net_15',
+  id: 'NET_15',
   label: 'Net 15',
 }, {
-  id: 'net_30',
+  id: 'NET_30',
   label: 'Net 30',
 }, {
-  id: 'net_60',
+  id: 'NET_60',
   label: 'Net 60',
 }]
 
-export type PaymentAllocationMethod =
-  | 'CASH'
-  | 'CHECK'
-  | 'CREDIT_CARD'
-  | 'ACH'
-  | 'REDEEMED_PREPAYMENT'
-  | 'OTHER'
-
 export type Bill = {
-  id: string
-  type: 'Bill'
-  business_id: string
-  external_id: string
-  status: BillStatus
-  received_at: string
-  due_at: string
-  paid_at?: string
-  voided_at?: string
+  additional_sales_taxes: SalesTax[]
+  additional_sales_taxes_total: number
   bill_number: string
-  line_items: BillLineItem[]
-  subtotal: number
-  total_amount: number
-  outstanding_balance: number
-  payment_allocations: BillPayment[]
+  business_id: string
+  due_at: string
+  external_id: string
+  id: string
   imported_at: string
-  updated_at: string
+  line_items: BillLineItem[]
+  outstanding_balance: number
+  paid_at?: string
+  payment_allocations: BillPaymentAllocation[]
+  received_at: string
+  status: BillStatus
+  subtotal: number
+  terms: BillTerm
+  total_amount: number
   transaction_tags: TransactionTag[]
+  type: 'Bill'
+  updated_at: string
+  voided_at?: string
   vendor?: Vendor
 }
 
-export type BillLineItem = {
-  id: string
-  external_id: string
-  bill_id: string
+type BillLineItem = {
   account_identifier: string
+  bill_id: string
   description: string
+  external_id: string
+  id: string
   product: string
-  unit_price: number
   quantity: number
+  sales_taxes: SalesTax[] | null
   subtotal: number
   total_amount: number
+  unit_price: number
 }
 
-export type BillPayment = {
-  id: string
-  external_id: string
-  at: string
-  method: PaymentAllocationMethod
-  fee: number
-  additional_fees: AdditionalFee[]
+type BillPaymentAllocation = {
+  bill_id: string
+  payment_id: string
   amount: number
-  processor?: string
-  imported_at: string
   transaction_tags: TransactionTag[]
 }
 
-export type AdditionalFee = {
-  fee_amount: number
-  description: string
-  account: string
+type SalesTax = {
+  amount: number
+  tax_account: TaxAccount
+}
+
+type TaxAccount = {
+  id?: string
+  name?: string
+  type?: string
 }
