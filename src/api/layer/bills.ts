f6af4fbe8ca51e7b90c4f624025fb56d38eacde6
Tom Antas <tom@altalogy.com>
2025-02-17 15:00:44 +0100

    switching to api

--- src/api/layer/bills.ts
+++ src/api/layer/bills.ts
@@ -2,5 +2,6 @@ import { Bill } from '../../types/bills'
 import { get } from './authenticated_http'
 
 export const getBills = get<{ data: Bill[] }>(
-  ({ businessId }) => `/v1/businesses/${businessId}/bills`,
+  ({ businessId, startDate, endDate, status }) => `/v1/businesses/${businessId}/bills?received_at_start=${startDate}&received_at_end=${endDate}&status=${status}`,
+  // ({ businessId, startDate, endDate }) => `/v1/businesses/${businessId}/bills?status=PAID`,
 )
