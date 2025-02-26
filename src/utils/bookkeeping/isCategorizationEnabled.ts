d32f903e1021e02b8ec08575d5e7f762e7bca558
Kesin Ryan Dehejia <kesin@layerfi.com>
2025-02-26 15:00:09 -0800

    fix: `BOOKKEEPING_PAUSED` should allow users to categorize

--- src/utils/bookkeeping/isCategorizationEnabled.ts
+++ src/utils/bookkeeping/isCategorizationEnabled.ts
@@ -3,10 +3,9 @@ import type { BookkeepingStatus } from '../../hooks/bookkeeping/useBookkeepingSt
 export function isCategorizationEnabledForStatus(status: BookkeepingStatus) {
   switch (status) {
     case 'NOT_PURCHASED':
+    case 'BOOKKEEPING_PAUSED':
       return true
     case 'ACTIVE':
-      return false
-    case 'BOOKKEEPING_PAUSED':
       return false
   }
 }
