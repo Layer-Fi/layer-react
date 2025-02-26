d7cca907d71d23bd99769ca8b5d2d3a6e83c292f
Tom <tom@altalogy.com>
2025-02-26 09:55:54 +0100

    feat: small changes from bills pr (part 2) (#505)

--- src/components/Header/HeaderRow.tsx
+++ src/components/Header/HeaderRow.tsx
@@ -1,14 +1,20 @@
 import { CSSProperties, ReactNode } from 'react'
 import classNames from 'classnames'
+import { toDataProperties } from '../../utils/styleUtils/toDataProperties'
 
 interface HeaderRowProps {
   className?: string
   style?: CSSProperties
   children: ReactNode
+  direction?: 'row' | 'col'
 }
 
-export const HeaderRow = ({ className, children, style }: HeaderRowProps) => (
-  <div className={classNames('Layer__header__row', className)} style={style}>
-    {children}
-  </div>
-)
+export const HeaderRow = ({ className, children, direction, style }: HeaderRowProps) => {
+  const dataProps = toDataProperties({ direction })
+
+  return (
+    <div {...dataProps} className={classNames('Layer__header__row', className)} style={style}>
+      {children}
+    </div>
+  )
+}
