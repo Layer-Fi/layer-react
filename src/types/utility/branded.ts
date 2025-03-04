d43ea0b7572f82a62c3212af7759e5c45792ab0a
Kesin Ryan Dehejia <kesin@layerfi.com>
2025-03-03 16:29:52 -0800

    fix: define branded types in same file as symbol

--- src/types/utility/branded.ts
+++ src/types/utility/branded.ts
@@ -6,4 +6,11 @@ type LayerBrand<in out ID extends string | symbol> = {
   }
 }
 
-export type LayerBranded<T, ID extends string | symbol> = T & LayerBrand<ID>
+type LayerBranded<T, ID extends string | symbol> = T & LayerBrand<ID>
+
+/*
+ * It appears that `npm-dts` does not like constructing branded types outside of the
+ * file where the symbol is declared.
+ */
+export type EmailAddress = LayerBranded<string, 'EmailAddress'>
+export type PhoneNumber = LayerBranded<string, 'PhoneNumber'>
