85dd0b02fcd310fad726fc61fb969f80d832d6e3
Tom Antas <tom@altalogy.com>
2025-03-07 12:32:46 +0100

    drop notifications

--- src/api/layer/notifications.ts
+++
@@ -1,6 +0,0 @@
-import { RawNotificationsData } from '../../hooks/notifications/types'
-import { get } from './authenticated_http'
-
-export const getNotifications = get<{ data: RawNotificationsData }>(
-  ({ businessId }) => `/v1/businesses/${businessId}/notifications`,
-)
--- src/hooks/notifications/types.ts
+++
@@ -1,6 +0,0 @@
-export type RawNotificationsData = {
-  uncategorized_transactions_this_month: number
-  uncategorized_transactions_all_time: number
-  todo_tasks: number
-  external_accounts_requiring_update: number
-}
