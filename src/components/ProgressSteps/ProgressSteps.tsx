4d2a595a5c7c178a1a20fee1f7cd463df6bd5276
Tom <tom@altalogy.com>
2025-03-10 21:42:57 +0100

    feat: progress steps component (#526)

---
+++ src/components/ProgressSteps/ProgressSteps.tsx
@@ -0,0 +1,36 @@
+import { Text, TextSize } from '../Typography'
+import Check from '../../icons/Check'
+import { toDataProperties } from '../../utils/styleUtils/toDataProperties'
+
+export type ProgressStepsProps = {
+  steps: string[]
+  currentStep: number
+}
+
+export const ProgressSteps = ({ steps, currentStep }: ProgressStepsProps) => {
+  return (
+    <div className='Layer__progress-steps'>
+      {steps.map((step, index) => {
+        const dataProperties = toDataProperties({ active: index <= currentStep, complete: currentStep > index })
+
+        return (
+          <div
+            key={index}
+            className='Layer__progress-steps--step-wrapper'
+          >
+            <div className='Layer__progress-steps--step' {...dataProperties}>
+              <div className='Layer__progress-steps--circle'>
+                {index < currentStep && <Check size={10} />}
+              </div>
+              <div className='Layer__progress-steps--label'>
+                <Text size={TextSize.sm}>{step}</Text>
+              </div>
+            </div>
+            {index < steps.length - 1 && <div className='Layer__progress-steps--step-line' />}
+          </div>
+        )
+      })}
+    </div>
+
+  )
+}
