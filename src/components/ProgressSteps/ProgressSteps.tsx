24d103ab2d61027bc6b34277d4bbdd156f6f741f
Tom Antas <tom@altalogy.com>
2025-03-10 15:56:47 +0100

    switch to data props

--- src/components/ProgressSteps/ProgressSteps.tsx
+++ src/components/ProgressSteps/ProgressSteps.tsx
@@ -1,6 +1,6 @@
-import classNames from 'classnames'
 import { Text, TextSize } from '../Typography'
 import Check from '../../icons/Check'
+import { toDataProperties } from '../../utils/styleUtils/toDataProperties'
 
 export type ProgressStepsProps = {
   steps: string[]
@@ -11,32 +11,14 @@ export const ProgressSteps = ({ steps, currentStep }: ProgressStepsProps) => {
   return (
     <div className='Layer__progress-steps'>
       {steps.map((step, index) => {
-        const stepClass = classNames(
-          'Layer__progress-steps--step',
-          {
-            'Layer__progress-steps--step-active':
-              index <= currentStep,
-          },
-          {
-            'Layer__progress-steps--step-complete':
-              currentStep > index,
-          },
-        )
-
-        const lineClass = classNames(
-          'Layer__progress-steps--step-line',
-          {
-            'Layer__progress-steps--step-line-filled':
-              currentStep > index,
-          },
-        )
+        const dataProperties = toDataProperties({ active: index <= currentStep, complete: currentStep > index })
 
         return (
           <div
             key={index}
             className='Layer__progress-steps--step-wrapper'
           >
-            <div className={stepClass}>
+            <div className='Layer__progress-steps--step' {...dataProperties}>
               <div className='Layer__progress-steps--circle'>
                 {index < currentStep && <Check size={10} />}
               </div>
@@ -44,7 +26,7 @@ export const ProgressSteps = ({ steps, currentStep }: ProgressStepsProps) => {
                 <Text size={TextSize.sm}>{step}</Text>
               </div>
             </div>
-            {index < steps.length - 1 && <div className={lineClass} />}
+            {index < steps.length - 1 && <div className='Layer__progress-steps--step-line' />}
           </div>
         )
       })}
