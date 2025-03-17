d48adfb41a511384aea491fbbde72a0385e0ac92
Kesin Ryan Dehejia <kesin@layerfi.com>
2025-03-17 11:11:10 -0700

    feat: `useBookkeepingPeriods` hook

---
+++ src/hooks/bookkeeping/periods/useBookkeepingPeriods.ts
@@ -0,0 +1,97 @@
+import useSWR from 'swr'
+import { useLayerContext } from '../../../contexts/LayerContext'
+import { useAuth } from '../../useAuth'
+import { get } from '../../../api/layer/authenticated_http'
+import {
+  isActiveBookkeepingStatus,
+  useBookkeepingStatus,
+} from '../useBookkeepingStatus'
+import type { Task } from '../../../types/tasks'
+import type { EnumWithUnknownValues } from '../../../types/utility/enumWithUnknownValues'
+
+const BOOKKEEPING_PERIOD_STATUSES = [
+  'BOOKKEEPING_NOT_PURCHASED',
+  'ONBOARDING',
+  'CLOSING_NOT_STARTED',
+  'CLOSING_IN_PROGRESS',
+  'PROVISIONALLY_COMPLETE',
+  'CLOSED',
+] as const
+
+export type BookkeepingPeriodStatus = typeof BOOKKEEPING_PERIOD_STATUSES[number]
+type RawBookkeepingPeriodStatus = EnumWithUnknownValues<BookkeepingPeriodStatus>
+
+function constrainToKnownBookkeepingPeriodStatus(status: string): BookkeepingPeriodStatus {
+  if (BOOKKEEPING_PERIOD_STATUSES.includes(status as BookkeepingPeriodStatus)) {
+    return status as BookkeepingPeriodStatus
+  }
+
+  return 'BOOKKEEPING_NOT_PURCHASED'
+}
+
+type BookkeepingPeriod = {
+  id: string
+  month: number
+  year: number
+  status: RawBookkeepingPeriodStatus
+  tasks: ReadonlyArray<Task>
+}
+
+const getBookkeepingPeriods = get<
+  {
+    data: {
+      periods: ReadonlyArray<BookkeepingPeriod>
+    }
+  },
+  { businessId: string }
+>(({ businessId }) => {
+  return `/v1/businesses/${businessId}/bookkeeping/periods`
+})
+
+function buildKey({
+  access_token: accessToken,
+  apiUrl,
+  businessId,
+  isActive,
+}: {
+  access_token?: string
+  apiUrl?: string
+  businessId: string
+  isActive: boolean
+}) {
+  if (accessToken && apiUrl && isActive) {
+    return {
+      accessToken,
+      apiUrl,
+      businessId,
+      tags: ['#bookkeeping', '#periods'],
+    } as const
+  }
+}
+
+export function useBookkeepingPeriods() {
+  const { data: auth } = useAuth()
+  const { businessId } = useLayerContext()
+
+  const { data } = useBookkeepingStatus()
+  const isActive = data ? isActiveBookkeepingStatus(data.status) : false
+
+  return useSWR(
+    () => buildKey({
+      ...auth,
+      businessId,
+      isActive,
+    }),
+    ({ accessToken, apiUrl, businessId }) => getBookkeepingPeriods(
+      apiUrl,
+      accessToken,
+      { params: { businessId } },
+    )()
+      .then(
+        ({ data: { periods } }) => periods.map(period => ({
+          ...period,
+          status: constrainToKnownBookkeepingPeriodStatus(period.status),
+        })),
+      ),
+  )
+}
