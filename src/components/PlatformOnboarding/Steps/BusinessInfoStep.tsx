6fc5fdfc6fce3ec1af3a708b77da4127365b1571
Tom Antas <tom@altalogy.com>
2025-03-18 15:00:02 +0100

    add heading to business form step

--- src/components/PlatformOnboarding/Steps/BusinessInfoStep.tsx
+++ src/components/PlatformOnboarding/Steps/BusinessInfoStep.tsx
@@ -1,12 +1,17 @@
 import { BusinessForm } from '../../BusinessForm/BusinessForm'
+import { Heading } from '../../ui/Typography/Heading'
 
 type BusinessInfoStepProps = {
+  title?: string
   onNext: () => void
 }
 
-export const BusinessInfoStep = ({ onNext }: BusinessInfoStepProps) => {
+const defaultTitle = 'We’ll use this information to contact you whenever we have questions on your books.'
+
+export const BusinessInfoStep = ({ title = defaultTitle, onNext }: BusinessInfoStepProps) => {
   return (
     <>
+      <Heading>{title}</Heading>
       <BusinessForm onSuccess={onNext} />
     </>
   )
