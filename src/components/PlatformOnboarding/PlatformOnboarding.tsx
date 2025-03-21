64a6e63aba34f91888fdba01e0f434b56ed0d92e
Tom Antas <tom@altalogy.com>
2025-03-21 14:25:05 +0100

    clean to basic variant

--- src/components/PlatformOnboarding/PlatformOnboarding.tsx
+++ src/components/PlatformOnboarding/PlatformOnboarding.tsx
@@ -19,53 +19,39 @@ const PLATFORM_ONBOARDING_STEPS = [
     id: 'link-accounts',
     label: 'Connect accounts',
   },
-  //   {
-  //     id: 'schedule-onboarding',
-  //     label: 'Schedule onboarding',
-  //   },
   {
     id: 'summary',
     label: 'Summary',
-    static: true,
   },
 ]
-
-const defaultEnabledSteps = ['welcome', 'business-info', 'link-accounts', 'summary']
-// const defaultEnabledSteps = ['link-accounts']
 
 type PlatformOnboardingStepKey = typeof PLATFORM_ONBOARDING_STEPS[number]['id']
 
 type PlatformOnboardingProps = {
-  steps?: PlatformOnboardingStepKey[]
+  onComplete?: () => void
 }
 
-export const PlatformOnboarding = ({ steps = defaultEnabledSteps }: PlatformOnboardingProps) => {
-  const stepsList = PLATFORM_ONBOARDING_STEPS.filter(step => steps.includes(step.id))
-  const [step, setStep] = useState<PlatformOnboardingStepKey>(stepsList[0].id)
+export const PlatformOnboarding = ({ onComplete }: PlatformOnboardingProps) => {
+  const [step, setStep] = useState<PlatformOnboardingStepKey>(PLATFORM_ONBOARDING_STEPS[0].id)
 
-  const isFirstStep = stepsList[0].id === step
-
-  const onOnboardingComplete = () => {
-    /** @TODO - save onboarding as completed */
-    alert('onboarding complete')
-  }
+  const isFirstStep = PLATFORM_ONBOARDING_STEPS[0].id === step
 
   const nextStep = () => {
-    const currentStepIndex = stepsList.findIndex(s => s.id === step)
-    if (currentStepIndex === stepsList.length - 1) {
-      onOnboardingComplete()
+    const currentStepIndex = PLATFORM_ONBOARDING_STEPS.findIndex(s => s.id === step)
+    if (currentStepIndex === PLATFORM_ONBOARDING_STEPS.length - 1) {
+      onComplete?.()
       return
     }
 
-    const nextStep = stepsList[currentStepIndex + 1]
+    const nextStep = PLATFORM_ONBOARDING_STEPS[currentStepIndex + 1]
     if (nextStep) {
       setStep(nextStep.id)
     }
   }
 
   const previousStep = () => {
-    const currentStepIndex = stepsList.findIndex(s => s.id === step)
-    const previousStep = stepsList[currentStepIndex - 1]
+    const currentStepIndex = PLATFORM_ONBOARDING_STEPS.findIndex(s => s.id === step)
+    const previousStep = PLATFORM_ONBOARDING_STEPS[currentStepIndex - 1]
     if (previousStep) {
       setStep(previousStep.id)
     }
@@ -74,7 +60,7 @@ export const PlatformOnboarding = ({ steps = defaultEnabledSteps }: PlatformOnbo
   const renderStepContent = () => {
     switch (step) {
       case 'welcome':
-        return <WelcomeStep onNext={nextStep} stepsEnabled={stepsList.map(s => s.id)} />
+        return <WelcomeStep onNext={nextStep} stepsEnabled={PLATFORM_ONBOARDING_STEPS.map(s => s.id)} />
       case 'business-info':
         return <BusinessInfoStep onNext={nextStep} />
       case 'link-accounts':
@@ -104,10 +90,10 @@ export const PlatformOnboarding = ({ steps = defaultEnabledSteps }: PlatformOnbo
           </div>
         )}
         <div className='Layer__platfom-onboarding-layout__box'>
-          {stepsList.length > 1 && (
+          {PLATFORM_ONBOARDING_STEPS.length > 1 && (
             <ProgressSteps
-              steps={stepsList.map(step => step.label)}
-              currentStep={step === 'summary' ? stepsList.length : stepsList.findIndex(s => s.id === step)}
+              steps={PLATFORM_ONBOARDING_STEPS.map(step => step.label)}
+              currentStep={step === 'summary' ? PLATFORM_ONBOARDING_STEPS.length : PLATFORM_ONBOARDING_STEPS.findIndex(s => s.id === step)}
             />
           )}
           <div className='Layer__platform-onboarding-layout__content'>
