fe1ad8c5ebebc0070a1eedd0b3aa68aed626b547
Tom Antas <tom@altalogy.com>
2025-03-31 16:52:31 +0200

    clean tasksheader

--- src/components/Tasks/TasksHeader.tsx
+++ src/components/Tasks/TasksHeader.tsx
@@ -1,22 +1,13 @@
 import { Text, TextSize } from '../Typography'
-import classNames from 'classnames'
 
 export const TasksHeader = ({
   tasksHeader = 'Bookkeeping Tasks',
-  collapsable,
 }: {
   tasksHeader?: string
-  collapsable?: boolean
-  open?: boolean
-  toggleContent: () => void
 }) => {
   return (
-    <div className={classNames('Layer__tasks-header', collapsable && 'Layer__tasks-header--collapsable')}>
-      <div className='Layer__tasks-header__left-col'>
-        <div className='Layer__tasks-header__left-col__title'>
-          <Text size={TextSize.lg}>{tasksHeader}</Text>
-        </div>
-      </div>
+    <div className='Layer__tasks-header'>
+      <Text size={TextSize.lg}>{tasksHeader}</Text>
     </div>
   )
 }
