0e22f6c451cd14270a31d53952c0791a22ef4d8f
Tom Antas <tom@altalogy.com>
2025-04-03 16:58:42 +0200

    notifications

--- src/hooks/bookkeeping/periods/useBookkeepingPeriodStatus.ts
+++ src/hooks/bookkeeping/periods/useBookkeepingPeriodStatus.ts
@@ -4,14 +4,16 @@ import { useBookkeepingPeriods } from './useBookkeepingPeriods'
 export const useBookkeepingPeriodStatus = ({ currentMonthDate }: { currentMonthDate: Date }) => {
   const { data, isLoading } = useBookkeepingPeriods()
 
-  const status = useMemo(() => {
+  const currentMonthData = useMemo(() => {
     const currentMonth = currentMonthDate.getMonth() + 1
     const currentYear = currentMonthDate.getFullYear()
 
     return data?.find(
       period => period.year === currentYear && period.month === currentMonth,
-    )?.status
+    )
   }, [data, currentMonthDate])
 
-  return { status, isLoading }
+  const status = currentMonthData?.status
+
+  return { data: currentMonthData, status, isLoading }
 }
