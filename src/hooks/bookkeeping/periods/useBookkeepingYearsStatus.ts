cfca6bc6e5d1ff514e4133d5cd5858d8f255d3b2
Kesin Ryan Dehejia <kesin@layerfi.com>
2025-04-09 17:34:52 -0700

    fix: hide years without any active bookkeeping period

--- src/hooks/bookkeeping/periods/useBookkeepingYearsStatus.ts
+++ src/hooks/bookkeeping/periods/useBookkeepingYearsStatus.ts
@@ -4,6 +4,7 @@ import { useLayerContext } from '../../../contexts/LayerContext/LayerContext'
 import { getActivationDate } from '../../../utils/business'
 import { getYear } from 'date-fns'
 import { isIncompleteTask, type UserVisibleTask } from '../../../utils/bookkeeping/tasks/bookkeepingTasksFilters'
+import { isActiveBookkeepingPeriod } from '../../../utils/bookkeeping/periods/getFilteredBookkeepingPeriods'
 
 export const useBookkeepingYearsStatus = () => {
   const { business } = useLayerContext()
@@ -16,22 +17,23 @@ export const useBookkeepingYearsStatus = () => {
     const currentYear: number = new Date().getFullYear()
     const count: number = currentYear - startYear + 1
 
-    return Array.from({ length: count }, (_, index) => ({
-      year: startYear + index,
-    })).map((d) => {
-      const tasks = data
-        ?.filter(period => period.year === d.year)
-        .reduce((acc, period) => acc.concat(period.tasks), [] as Array<UserVisibleTask>)
+    return Array.from({ length: count }, (_, index) => ({ year: startYear + index }))
+      .map(({ year }) => {
+        const tasks = data
+          ?.filter(period => period.year === year)
+          .reduce((acc, period) => acc.concat(period.tasks), [] as Array<UserVisibleTask>)
 
-      const unresolvedTasks = tasks?.filter(task => isIncompleteTask(task)).length
+        const unresolvedTaskCount = tasks?.filter(task => isIncompleteTask(task)).length
 
-      return {
-        ...d,
-        tasks,
-        unresolvedTasks,
-        completed: Boolean(!unresolvedTasks),
-      }
-    }).sort((a, b) => b.year - a.year)
+        return {
+          year,
+          tasks,
+          unresolvedTasks: unresolvedTaskCount,
+          completed: unresolvedTaskCount === 0,
+        }
+      })
+      .filter(({ year }) => data?.some(period => period.year === year && isActiveBookkeepingPeriod(period)))
+      .sort((a, b) => b.year - a.year)
   }, [activationDate, data])
 
   const anyPreviousYearIncomplete = yearStatuses?.find(year => !year.completed && year.year < new Date().getFullYear())
