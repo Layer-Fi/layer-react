2ed02364b5746cbeeafcf4ac41653725cabe5660
Daniel O'Neel <danieloneel@gmail.com>
2025-04-11 17:46:38 -0700

    remove bank icon

--- src/icons/BankIcon.tsx
+++
@@ -1,8 +0,0 @@
-import { IconSvgProps } from './types'
-import { PiBank } from 'react-icons/pi'
-
-const BankIcon = ({ size = 18, ...props }: IconSvgProps) => {
-  return <PiBank size={size} {...props} />
-}
-
-export default BankIcon
