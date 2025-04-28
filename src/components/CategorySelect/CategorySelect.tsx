43121c43303122d490787a0566612e81cfd0845f
Tom Antas <tom@altalogy.com>
2025-04-28 16:13:23 +0200

    refactor categoryselect file

--- src/components/CategorySelect/CategorySelect.tsx
+++ src/components/CategorySelect/CategorySelect.tsx
@@ -1,139 +1,28 @@
-import { Button, Popover, DialogTrigger, ListBox, ListBoxSection, Header, ListBoxItem, Collection } from 'react-aria-components'
+import { Button, Popover, DialogTrigger, ListBox, ListBoxSection } from 'react-aria-components'
 import { Input } from '../Input/Input'
-import { ReactNode, useMemo, useState } from 'react'
+import { useMemo, useState } from 'react'
 import { useLayerContext } from '../../contexts/LayerContext/LayerContext'
-import { BankTransaction, Category } from '../../types'
-import { CategoryOption, CategoryOptionPayload, CategoryWithHide, OptionActionType } from './types'
-import { buildMatchOptions, buildSuggestedOptions, filterCategories, findParentCategory } from './utils'
-import { Text, TextSize, TextWeight } from '../Typography'
+import { CategoryOption, CategorySelectProps, CategoryWithHide } from './types'
+import { buildMatchOptions, buildSuggestedOptions, filterCategories } from './utils'
+import { Text } from '../Typography'
 import { CategorySelectDrawer } from './CategorySelectDrawer'
 import classNames from 'classnames'
 import pluralize from 'pluralize'
 import ChevronDown from '../../icons/ChevronDown'
-import { Tooltip, TooltipContent, TooltipTrigger } from '../Tooltip/Tooltip'
-import InfoIcon from '../../icons/InfoIcon'
-import { parseISO, format as formatTime } from 'date-fns'
-import { centsToDollars as formatMoney } from '../../models/Money'
-import { DATE_FORMAT } from '../../config/general'
 import { Badge, BadgeSize } from '../Badge/Badge'
 import MinimizeTwo from '../../icons/MinimizeTwo'
-import { VStack } from '../ui/Stack/Stack'
-import CheckIcon from '../../icons/Check'
+import { CategoriesList } from './components/CategoriesList'
+import { MatchesList } from './components/MatchesList'
+import { MenuSection } from './components/MenuSection'
+import { SuggestionsList } from './components/SuggestionsList'
 
-type CategorySelectProps = {
-  name?: string
-  bankTransaction: BankTransaction
-  value: CategoryOption | undefined
-  onChange: (newValue: CategoryOption) => void
-  disabled?: boolean
-  className?: string
-  showTooltips: boolean
-  excludeMatches?: boolean
-  asDrawer?: boolean
-}
-
-const MAX_INDENT_LEVEL = 3
-
-const MenuSection = ({ children }: { children: ReactNode }) => (
-  <div className='Layer__category-select__menu-section'>{children}</div>
-)
-
-/* @TODO - unlock when Adding category is available
+/* @TODO - uncomment when adding category is available
 const MenuItem = ({ text, icon }: { text: string, icon?: ReactNode }) => (
   <div className='Layer__category-select__menu-item'>
     <Text>{text}</Text>
     {icon}
   </div>
 ) */
-
-type CategoryListProps = {
-  option: CategoryWithHide
-  level: number
-  accountName: string
-  onSelect: (newValue: CategoryOption) => void
-  selected: CategoryOption | undefined
-  showTooltips: CategorySelectProps['showTooltips']
-}
-
-const CategoryList = ({ option, level = 0, accountName, onSelect, selected, showTooltips }: CategoryListProps) => {
-  if (option?.subCategories) {
-    return (
-      <>
-        <Header
-          style={{
-            display: option.hide ? 'none' : 'flex',
-            paddingLeft: `${(Math.min(MAX_INDENT_LEVEL, Math.max(level, 1)) * 12) + 8}px`,
-          }}
-        >
-          {level === 0
-            ? <Text size={TextSize.xs} status='disabled'>{option.display_name}</Text>
-            : <Text weight={TextWeight.bold}>{option.display_name}</Text>}
-        </Header>
-        <ListBoxSection className='Layer__category-select__list-box-section' key={`${accountName}-${option.category}-section`} style={{ display: (option).hide ? 'none' : 'flex' }}>
-          {option.subCategories.map((o, i) => (
-            <Collection key={`${accountName}-${option.category}-section-${i}`} items={o.subCategories ?? []}>
-              <CategoryList
-                option={o}
-                level={level + 1}
-                accountName={option.display_name}
-                onSelect={onSelect}
-                selected={selected}
-                showTooltips={showTooltips}
-              />
-            </Collection>
-          ))}
-        </ListBoxSection>
-      </>
-    )
-  }
-
-  return (
-    <ListBoxItem
-      key={`${accountName}-${option.category}`}
-      className='Layer__category-select__list-item'
-      style={{
-        paddingLeft: `${8 + (Math.min(MAX_INDENT_LEVEL, level) * 12)}px`,
-        display: option.hide ? 'none' : 'grid',
-      }}
-      textValue={option?.display_name}
-      onAction={() => {
-        onSelect({
-          type: 'category',
-          payload: {
-            ...option,
-            option_type: 'category' as OptionActionType,
-            subCategories: option.subCategories as Category[],
-            id: 'id' in option ? option.id : undefined,
-            stable_name: 'stable_name' in option ? option.stable_name : undefined,
-          } as CategoryOptionPayload,
-        })
-      }}
-    >
-      <Text slot='name'>{option?.display_name}</Text>
-
-      <span slot='account'>
-        <Text size={TextSize.sm} status='disabled' ellipsis>{accountName}</Text>
-      </span>
-
-      {option.description && (
-        <div className='Layer__select__option-menu--tooltip' slot='tooltip' onClick={e => e.stopPropagation()}>
-          <Tooltip>
-            <TooltipTrigger>
-              <InfoIcon />
-            </TooltipTrigger>
-            <TooltipContent>
-              {option.description}
-            </TooltipContent>
-          </Tooltip>
-        </div>
-      )}
-
-      {'id' in option && selected?.payload.id === option.id && (
-        <CheckIcon slot='icon' size={12} />
-      )}
-    </ListBoxItem>
-  )
-}
 
 export const CategorySelect = ({
   bankTransaction,
@@ -204,68 +93,19 @@ export const CategorySelect = ({
             onChange={e => setSearch((e.target as HTMLInputElement).value)}
             aria-label='Search category'
           />
-          {(matches && matches.length > 0)
-          && (
-            <MenuSection>
-              <ListBox slot='listbox' aria-label='Matches'>
-                <ListBoxSection>
-                  <Header slot='header'>
-                    <Text size={TextSize.xs} status='disabled'>Match</Text>
-                  </Header>
-                  {matches?.map((option, index) => (
-                    <ListBoxItem className='Layer__category-select__ms-list-item' key={index} textValue={option.payload.display_name} onAction={() => onSelect(option)}>
-                      <VStack slot='label'>
-                        <Text slot='date-and-amount' size={TextSize.sm}>
-                          {option.payload.date && formatTime(parseISO(option.payload.date), DATE_FORMAT)}
-                          {' '}
-                          |
-                          {' '}
-                          $
-                          {formatMoney(option.payload.amount)}
-                        </Text>
-                        <Text slot='name'>{option.payload.display_name}</Text>
-                      </VStack>
-                      {value?.payload?.id === option.payload.id && (
-                        <span slot='icon'>
-                          <CheckIcon size={12} />
-                        </span>
-                      )}
-                    </ListBoxItem>
-                  ),
-                  )}
-                </ListBoxSection>
-              </ListBox>
-            </MenuSection>
-          )}
 
-          {(suggestions && suggestions.length > 0)
-          && (
-            <MenuSection>
-              <ListBox slot='listbox' aria-label='Suggestions'>
-                <ListBoxSection>
-                  <Header slot='header'>
-                    <Text size={TextSize.xs} status='disabled'>Suggestions</Text>
-                  </Header>
-                  {suggestions.map((option, index) => (
-                    <ListBoxItem className='Layer__category-select__ms-list-item' key={index} textValue={option.payload.display_name} onAction={() => onSelect(option)}>
-                      <VStack slot='label'>
-                        <Text slot='name'>{option.payload.display_name}</Text>
-                        <Text slot='account' size={TextSize.sm}>
-                          {findParentCategory(categories, option.payload.id)?.display_name}
-                        </Text>
-                      </VStack>
-                      {value?.payload?.id === option.payload.id && (
-                        <span slot='icon'>
-                          <CheckIcon size={12} />
-                        </span>
-                      )}
-                    </ListBoxItem>
-                  ),
-                  )}
-                </ListBoxSection>
-              </ListBox>
-            </MenuSection>
-          )}
+          <MatchesList
+            matches={matches}
+            onSelect={onSelect}
+            value={value}
+          />
+
+          <SuggestionsList
+            suggestions={suggestions}
+            categories={categories}
+            onSelect={onSelect}
+            value={value}
+          />
 
           <MenuSection>
             <ListBox
@@ -281,7 +121,7 @@ export const CategorySelect = ({
                     display: section.hide ? 'none' : 'flex',
                   }}
                 >
-                  <CategoryList
+                  <CategoriesList
                     option={section}
                     level={0}
                     accountName={section.display_name}
---
+++ src/components/CategorySelect/components/CategoriesList.tsx
@@ -0,0 +1,99 @@
+import { ListBoxSection, Header, ListBoxItem, Collection } from 'react-aria-components'
+import { CategoryOption, CategoryOptionPayload, CategoryWithHide, OptionActionType } from '../types'
+import { Text, TextSize, TextWeight } from '../../Typography'
+import { Tooltip, TooltipContent, TooltipTrigger } from '../../Tooltip/Tooltip'
+import InfoIcon from '../../../icons/InfoIcon'
+import CheckIcon from '../../../icons/Check'
+import { CategorySelectProps } from '../types'
+import { Category } from '../../../types'
+
+const MAX_INDENT_LEVEL = 3
+
+type CategoriesListProps = {
+  option: CategoryWithHide
+  level: number
+  accountName: string
+  onSelect: (newValue: CategoryOption) => void
+  selected: CategoryOption | undefined
+  showTooltips: CategorySelectProps['showTooltips']
+}
+
+export const CategoriesList = ({ option, level = 0, accountName, onSelect, selected, showTooltips }: CategoriesListProps) => {
+  if (option?.subCategories) {
+    return (
+      <>
+        <Header
+          style={{
+            display: option.hide ? 'none' : 'flex',
+            paddingLeft: `${(Math.min(MAX_INDENT_LEVEL, Math.max(level, 1)) * 12) + 8}px`,
+          }}
+        >
+          {level === 0
+            ? <Text size={TextSize.xs} status='disabled'>{option.display_name}</Text>
+            : <Text weight={TextWeight.bold}>{option.display_name}</Text>}
+        </Header>
+        <ListBoxSection className='Layer__category-select__list-box-section' key={`${accountName}-${option.category}-section`} style={{ display: (option).hide ? 'none' : 'flex' }}>
+          {option.subCategories.map((o, i) => (
+            <Collection key={`${accountName}-${option.category}-section-${i}`} items={o.subCategories ?? []}>
+              <CategoriesList
+                option={o}
+                level={level + 1}
+                accountName={option.display_name}
+                onSelect={onSelect}
+                selected={selected}
+                showTooltips={showTooltips}
+              />
+            </Collection>
+          ))}
+        </ListBoxSection>
+      </>
+    )
+  }
+
+  return (
+    <ListBoxItem
+      key={`${accountName}-${option.category}`}
+      className='Layer__category-select__list-item'
+      style={{
+        paddingLeft: `${8 + (Math.min(MAX_INDENT_LEVEL, level) * 12)}px`,
+        display: option.hide ? 'none' : 'grid',
+      }}
+      textValue={option?.display_name}
+      onAction={() => {
+        onSelect({
+          type: 'category',
+          payload: {
+            ...option,
+            option_type: 'category' as OptionActionType,
+            subCategories: option.subCategories as Category[],
+            id: 'id' in option ? option.id : undefined,
+            stable_name: 'stable_name' in option ? option.stable_name : undefined,
+          } as CategoryOptionPayload,
+        })
+      }}
+    >
+      <Text slot='name'>{option?.display_name}</Text>
+
+      <span slot='account'>
+        <Text size={TextSize.sm} status='disabled' ellipsis>{accountName}</Text>
+      </span>
+
+      {option.description && (
+        <div className='Layer__select__option-menu--tooltip' slot='tooltip' onClick={e => e.stopPropagation()}>
+          <Tooltip>
+            <TooltipTrigger>
+              <InfoIcon />
+            </TooltipTrigger>
+            <TooltipContent>
+              {option.description}
+            </TooltipContent>
+          </Tooltip>
+        </div>
+      )}
+
+      {'id' in option && selected?.payload.id === option.id && (
+        <CheckIcon slot='icon' size={12} />
+      )}
+    </ListBoxItem>
+  )
+}
