659a9324c96cb8f82efe681d612aabee533af83f
Sarah Raines <sarah@layerfi.com>
2025-05-08 17:33:44 -0700

    Create new badge loader styles

--- src/components/BadgeLoader/BadgeLoader.tsx
+++ src/components/BadgeLoader/BadgeLoader.tsx
@@ -1,16 +1,25 @@
 import { ReactNode } from 'react'
 import LoaderIcon from '../../icons/Loader'
-
+import CheckIcon from '../../icons/Check'
+import XIcon from '../../icons/X'
+import AlertCircle from '../../icons/AlertCircle'
 export interface BadgeLoaderProps {
   children?: ReactNode
   size?: number
-  variant?: 'default' | 'info'
+  variant?: 'default' | 'info' | 'success' | 'error' | 'warning'
+}
+
+const BadgeLoaderIcon = ({ variant }: { variant: BadgeLoaderProps['variant'] }) => {
+  if (variant === 'success') return <CheckIcon size={12}/>
+  if (variant === 'error') return <XIcon size={12}/>
+  if (variant === 'warning') return <AlertCircle size={12}/>
+  return <LoaderIcon size={12} className='Layer__anim--rotating' />
 }
 
 export const BadgeLoader = ({ children, variant = 'default' }: BadgeLoaderProps) => {
   return (
     <span className={`Layer__loader Layer__loader--as-badge Layer__loader--${variant}`}>
-      <LoaderIcon size={11} className='Layer__anim--rotating' />
+      <BadgeLoaderIcon variant={variant}/>
       {children}
     </span>
   )
