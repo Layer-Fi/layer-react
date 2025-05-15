a373ab86bbe82bf282e3e4b3b1973e927d2d1052
Sarah Raines <sarah@layerfi.com>
2025-05-15 00:40:43 -0700

    implement `CustomerFacingBookkeepingPeriodStatus` as a mapping of internal status + tasks to external status

---
+++ src/hooks/bookkeeping/periods/utils.ts
@@ -0,0 +1,34 @@
+import { BookkeepingPeriodStatus } from "./useBookkeepingPeriods"
+
+export enum CustomerFacingBookkeepingPeriodStatus {
+  BOOKS_IN_PROGRESS = 'BOOKS_IN_PROGRESS',
+  ACTION_REQUIRED = 'ACTION_REQUIRED',
+  BOOKS_COMPLETED = 'BOOKS_COMPLETED',
+}
+
+export function getCustomerFacingBookkeepingPeriodStatus(status: BookkeepingPeriodStatus, hasOpenTasks: boolean) {
+  switch (status) {
+    case BookkeepingPeriodStatus.NOT_STARTED: {
+      return CustomerFacingBookkeepingPeriodStatus.BOOKS_IN_PROGRESS
+    }
+    case BookkeepingPeriodStatus.CLOSING_IN_REVIEW:
+    case BookkeepingPeriodStatus.IN_PROGRESS_AWAITING_BOOKKEEPER:
+    case BookkeepingPeriodStatus.IN_PROGRESS_AWAITING_CUSTOMER: {
+      return hasOpenTasks 
+        ? CustomerFacingBookkeepingPeriodStatus.ACTION_REQUIRED
+        : CustomerFacingBookkeepingPeriodStatus.BOOKS_IN_PROGRESS
+    }
+    case BookkeepingPeriodStatus.CLOSED_OPEN_TASKS: {
+      return hasOpenTasks 
+        ? CustomerFacingBookkeepingPeriodStatus.ACTION_REQUIRED
+        : CustomerFacingBookkeepingPeriodStatus.BOOKS_COMPLETED
+    }
+    case BookkeepingPeriodStatus.CLOSED_COMPLETE: {
+      return CustomerFacingBookkeepingPeriodStatus.BOOKS_COMPLETED
+    }
+    case BookkeepingPeriodStatus.BOOKKEEPING_NOT_ACTIVE:
+    default: {
+      return undefined
+    }
+  }
+}
