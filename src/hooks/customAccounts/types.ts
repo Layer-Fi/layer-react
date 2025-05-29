2e120f544e957bd3c6935f925fd10852f49f6e04
Sarah Raines <sarah@layerfi.com>
2025-05-29 10:48:43 -0700

    Implement API hooks for getting and creating custom accounts

---
+++ src/hooks/customAccounts/types.ts
@@ -0,0 +1,37 @@
+export enum CustomAccountSubtype {
+  CHECKING = 'CHECKING',
+  SAVINGS = 'SAVINGS',
+  CREDIT_CARD = 'CREDIT_CARD',
+}
+
+export enum CustomAccountType {
+  DEPOSITORY = 'DEPOSITORY',
+  CREDIT = 'CREDIT',
+}
+
+export type RawCustomAccount = {
+  id: string;
+  external_id: string | null;
+  mask: string | null;
+  account_name: string;
+  institution_name: string | null;
+  account_type: string;
+  account_subtype: string;
+  created_at: string | null;
+  updated_at: string | null;
+  archived_at: string | null;
+  ledger_account_id: string;
+}
+
+export type CustomAccount = Pick<RawCustomAccount, 'id'> & {
+  externalId: RawCustomAccount['external_id'];
+  mask: RawCustomAccount['mask'];
+  accountName: RawCustomAccount['account_name'];
+  institutionName: RawCustomAccount['institution_name'];
+  accountType: RawCustomAccount['account_type'];
+  accountSubtype: RawCustomAccount['account_subtype'];
+  createdAt: RawCustomAccount['created_at'];
+  updatedAt: RawCustomAccount['updated_at'];
+  archivedAt: RawCustomAccount['archived_at'];
+  ledgerAccountId: RawCustomAccount['ledger_account_id'];
+}
---
+++ src/hooks/customAccounts/useCustomAccounts.ts
@@ -0,0 +1,57 @@
+import { useLayerContext } from '../../contexts/LayerContext'
+import { useAuth } from '../useAuth'
+import { get } from '../../api/layer/authenticated_http'
+import useSWR from 'swr'
+import { type RawCustomAccount } from './types'
+
+export const CUSTOM_ACCOUNTS_TAG_KEY = '#custom-accounts'
+
+function buildKey({
+  access_token: accessToken,
+  apiUrl,
+  businessId,
+}: {
+  access_token?: string
+  apiUrl?: string
+  businessId: string
+}) {
+  if (accessToken && apiUrl) {
+    return {
+      accessToken,
+      apiUrl,
+      businessId,
+      tags: [CUSTOM_ACCOUNTS_TAG_KEY],
+    } as const
+  }
+}
+
+const getCustomAccounts = get<
+  {
+    data: {
+      type: 'Custom_Accounts'
+      custom_accounts: RawCustomAccount[]
+    }
+  },
+  {
+    businessId: string
+  }
+>(({ businessId }) => `/v1/businesses/${businessId}/custom-accounts/`)
+
+export function useCustomAccounts() {
+  const { data } = useAuth()
+  const { businessId } = useLayerContext()
+
+  return useSWR(
+    () => buildKey({
+      ...data,
+      businessId,
+    }),
+    ({ accessToken, apiUrl, businessId }) => getCustomAccounts(
+      apiUrl,
+      accessToken,
+      {
+        params: { businessId },
+      },
+    )().then(({ data }) => data?.custom_accounts),
+  )
+}
