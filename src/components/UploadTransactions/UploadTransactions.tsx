3d0ed2edbe2678c39b2abd68e60ee25855f82fc1
Sarah Raines <sarah@layerfi.com>
2025-05-29 10:53:42 -0700

    Create UploadTransactions component that contains all prior components

---
+++ src/components/UploadTransactions/UploadTransactions.tsx
@@ -0,0 +1,33 @@
+import { UploadTransactionsSelectAccountStep } from './UploadTransactionsSelectAccountStep'
+import { Wizard } from '../Wizard/Wizard'
+import { Heading } from '../ui/Typography/Heading'
+import type { Awaitable } from '../../types/utility/promises'
+import { VStack } from '../ui/Stack/Stack'
+import { P } from '../ui/Typography/Text'
+
+type UploadTransactionsProps = {
+  onComplete?: () => Awaitable<void>
+}
+
+export function UploadTransactions({ onComplete }: UploadTransactionsProps) {
+  return (
+    <section className='Layer__component'>
+      <Wizard
+        Header={(
+        <VStack gap='xs'>
+          <Heading>
+            Upload bank transactions
+          </Heading>
+          <P pbe='xl' size='sm' variant='subtle'>
+            Add file downloaded from your bank account
+          </P>
+        </VStack>
+        )}
+        Footer={null}
+        onComplete={onComplete}
+      >
+        <UploadTransactionsSelectAccountStep />
+      </Wizard>
+    </section>
+  )
+}
