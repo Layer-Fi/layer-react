6d36bd176dae89479d3a200895a6687fef86a54c
Sarah Raines <sarah@layerfi.com>
2025-06-06 09:33:01 -0700

    Add new text styles

--- src/components/ui/Typography/Text.tsx
+++ src/components/ui/Typography/Text.tsx
@@ -2,29 +2,117 @@ import { forwardRef } from 'react'
 import type { ComponentPropsWithoutRef, PropsWithChildren } from 'react'
 import { toDataProperties } from '../../../utils/styleUtils/toDataProperties'
 import type { Spacing } from '../sharedUITypes'
-import { Text as ReactAriaText, Label as ReactAriaLabel } from 'react-aria-components'
-
-const P_CLASS_NAME = 'Layer__P'
-
-type ParagraphProps = Pick<ComponentPropsWithoutRef<'p'>, 'slot'>
+import {
+  Header as ReactAriaHeader,
+  Label as ReactAriaLabel,
+  Text as ReactAriaText,
+} from 'react-aria-components'
 
 type TextStyleProps = {
   align?: 'center'
+  ellipsis?: true
+  noWrap?: true
+  pb?: Spacing
   pbe?: Spacing
   pbs?: Spacing
   size?: 'xs' | 'sm' | 'md' | 'lg'
+  status?: 'error'
   variant?: 'subtle'
+  weight?: 'bold'
 }
 
-export const P = forwardRef<HTMLParagraphElement, PropsWithChildren<ParagraphProps & TextStyleProps>>(
-  function P({ align, children, pbe, pbs, size, variant, ...restProps }, ref) {
-    const dataProperties = toDataProperties({
+type TextRenderingProps = {
+  nonAria?: true
+}
+
+function splitTextProps<TRest>(props: PropsWithChildren<TextStyleProps & TextRenderingProps & TRest>) {
+  const {
+    align,
+    children,
+    ellipsis,
+    nonAria,
+    noWrap,
+    pb,
+    pbe,
+    pbs,
+    size,
+    status,
+    variant,
+    weight,
+    ...restProps
+  } = props
+
+  return {
+    children,
+    dataProperties: toDataProperties({
       align,
+      ellipsis,
+      'no-wrap': noWrap,
+      pb,
       pbe,
       pbs,
       size,
+      status,
       variant,
-    })
+      weight,
+    }),
+    renderingProps: { nonAria },
+    restProps,
+  }
+}
+
+const HEADER_CLASS_NAME = 'Layer__Header'
+type HeaderProps = Pick<ComponentPropsWithoutRef<'header'>, 'id' | 'slot'> & TextRenderingProps
+
+export const Header = forwardRef<HTMLElementTagNameMap['header'], PropsWithChildren<HeaderProps & TextStyleProps>>(
+  function Header(props, ref) {
+    const { children, dataProperties, renderingProps, restProps } = splitTextProps(props)
+
+    const HeaderComponent = renderingProps.nonAria
+      ? 'header'
+      : ReactAriaHeader
+
+    return (
+      <HeaderComponent {...restProps} {...dataProperties} className={HEADER_CLASS_NAME} ref={ref}>
+        {children}
+      </HeaderComponent>
+    )
+  },
+)
+
+const LABEL_CLASS_NAME = 'Layer__Label'
+type LabelProps = Pick<ComponentPropsWithoutRef<'label'>, 'id' | 'slot' | 'htmlFor'> & TextRenderingProps
+
+export const Label = forwardRef<HTMLLabelElement, PropsWithChildren<LabelProps & TextStyleProps>>(
+  function Label(props, ref) {
+    const { children, dataProperties, renderingProps, restProps } = splitTextProps(props)
+
+    const LabelComponent = renderingProps.nonAria
+      ? 'label'
+      : ReactAriaLabel
+
+    return (
+      <LabelComponent {...restProps} {...dataProperties} className={LABEL_CLASS_NAME} ref={ref}>
+        {children}
+      </LabelComponent>
+    )
+  },
+)
+
+const P_CLASS_NAME = 'Layer__P'
+type ParagraphProps = Pick<ComponentPropsWithoutRef<'p'>, 'id' | 'slot'> & TextRenderingProps
+
+export const P = forwardRef<HTMLParagraphElement, PropsWithChildren<ParagraphProps & TextStyleProps>>(
+  function P(props, ref) {
+    const { children, dataProperties, renderingProps, restProps } = splitTextProps(props)
+
+    if (renderingProps.nonAria) {
+      return (
+        <p {...restProps} {...dataProperties} className={P_CLASS_NAME} ref={ref}>
+          {children}
+        </p>
+      )
+    }
 
     return (
       <ReactAriaText elementType='p' {...restProps} {...dataProperties} className={P_CLASS_NAME} ref={ref}>
@@ -34,47 +122,25 @@ export const P = forwardRef<HTMLParagraphElement, PropsWithChildren<ParagraphPro
   },
 )
 
-type SpanProps = Pick<ComponentPropsWithoutRef<'span'>, 'slot'>
-
 const SPAN_CLASS_NAME = 'Layer__Span'
+type SpanProps = Pick<ComponentPropsWithoutRef<'span'>, 'id' | 'slot'> & TextRenderingProps
 
-export const Span = forwardRef<HTMLSpanElement, PropsWithChildren<SpanProps & TextStyleProps & { noWrap?: boolean }>>(
-  function Span({ align, children, noWrap, pbe, pbs, size, variant, ...restProps }, ref) {
-    const dataProperties = toDataProperties({
-      align,
-      'no-wrap': noWrap,
-      pbe,
-      pbs,
-      size,
-      variant,
-    })
+export const Span = forwardRef<HTMLSpanElement, PropsWithChildren<SpanProps & TextStyleProps>>(
+  function Span(props, ref) {
+    const { children, dataProperties, renderingProps, restProps } = splitTextProps(props)
+
+    if (renderingProps.nonAria) {
+      return (
+        <span {...restProps} {...dataProperties} className={SPAN_CLASS_NAME} ref={ref}>
+          {children}
+        </span>
+      )
+    }
 
     return (
       <ReactAriaText {...restProps} {...dataProperties} className={SPAN_CLASS_NAME} ref={ref}>
         {children}
       </ReactAriaText>
-    )
-  },
-)
-
-const LABEL_CLASS_NAME = 'Layer__Label'
-
-type LabelProps = Pick<ComponentPropsWithoutRef<'label'>, 'slot' | 'htmlFor'>
-
-export const Label = forwardRef<HTMLLabelElement, PropsWithChildren<LabelProps & TextStyleProps>>(
-  function Label({ align, children, pbe, pbs, size, variant, ...restProps }, ref) {
-    const dataProperties = toDataProperties({
-      align,
-      pbe,
-      pbs,
-      size,
-      variant,
-    })
-
-    return (
-      <ReactAriaLabel {...restProps} {...dataProperties} className={LABEL_CLASS_NAME} ref={ref}>
-        {children}
-      </ReactAriaLabel>
     )
   },
 )
