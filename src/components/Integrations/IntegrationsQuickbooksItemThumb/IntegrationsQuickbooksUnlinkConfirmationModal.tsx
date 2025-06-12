ebbb529a09104aa92f2f5ef82ae6dfa4b6f26800
Sarah Raines <sarah@layerfi.com>
2025-06-11 23:36:29 -0700

    Rewrite Quickbooks unlink confirm modal with confirmation modal

--- src/components/Integrations/IntegrationsQuickbooksItemThumb/IntegrationsQuickbooksUnlinkConfirmationModal.tsx
+++ src/components/Integrations/IntegrationsQuickbooksItemThumb/IntegrationsQuickbooksUnlinkConfirmationModal.tsx
@@ -1,78 +1,21 @@
-import { Modal, ModalProps } from '../../ui/Modal/Modal'
-import { ModalContextBar, ModalHeading, ModalActions, ModalContent } from '../../ui/Modal/ModalSlots'
-import { Button, ButtonVariant } from '../../Button/Button'
-import { HStack, Spacer } from '../../ui/Stack/Stack'
-import { P } from '../../ui/Typography/Text'
-import { useCallback, useContext, useState } from 'react'
-import { RetryButton } from '../../Button'
+import { useContext } from 'react'
+import { ModalProps } from '../../ui/Modal/Modal'
 import { QuickbooksContext } from '../../../contexts/QuickbooksContext/QuickbooksContext'
-
-function IntegrationsQuickbooksUnlinkConfirmationModalContent({ onClose }: { onClose: () => void }) {
-  const { unlinkQuickbooks } = useContext(QuickbooksContext)
-
-  const [isProcessing, setIsProcessing] = useState(false)
-  const [hasFailed, setHasFailed] = useState(false)
-
-  const unlinkErrorText = 'Unlink failed. Check connection and retry in few seconds.'
-  const onClickUnlinkQuickbooks = useCallback(() => {
-    setIsProcessing(true)
-    unlinkQuickbooks()
-      .then(() => {
-        onClose()
-      })
-      .catch(() => {
-        setHasFailed(true)
-        setIsProcessing(false)
-      })
-  }, [unlinkQuickbooks, onClose])
-
-  return (
-    <>
-      <ModalContextBar onClose={onClose} />
-      <ModalHeading pbe='2xs'>
-        Unlink QuickBooks
-      </ModalHeading>
-      <ModalContent>
-        <P>Please confirm that you want to unlink QuickBooks.</P>
-      </ModalContent>
-      <ModalActions>
-        <HStack gap='md'>
-          <Spacer />
-          <Button variant={ButtonVariant.secondary} onClick={onClose}>
-            Cancel
-          </Button>
-          {!hasFailed
-            ? (
-              <Button
-                variant={ButtonVariant.primary}
-                onClick={onClickUnlinkQuickbooks}
-                isProcessing={isProcessing}
-                disabled={isProcessing}
-              >
-                Unlink QuickBooks
-              </Button>
-            )
-            : (
-              <RetryButton
-                onClick={onClickUnlinkQuickbooks}
-                processing={isProcessing}
-                disabled={isProcessing}
-                error={unlinkErrorText}
-              >
-                Retry Unlink QuickBooks
-              </RetryButton>
-            )}
-        </HStack>
-      </ModalActions>
-    </>
-  )
-}
+import { BaseConfirmationModal } from '../../BaseConfirmationModal/BaseConfirmationModal'
 
 type IntegrationsQuickbooksUnlinkConfirmationModalProps = Pick<ModalProps, 'isOpen' | 'onOpenChange'>
 export function IntegrationsQuickbooksUnlinkConfirmationModal({ isOpen, onOpenChange }: IntegrationsQuickbooksUnlinkConfirmationModalProps) {
+  const { unlinkQuickbooks } = useContext(QuickbooksContext)
+
   return (
-    <Modal flexBlock isOpen={isOpen} onOpenChange={onOpenChange}>
-      {({ close }) => <IntegrationsQuickbooksUnlinkConfirmationModalContent onClose={close} />}
-    </Modal>
+    <BaseConfirmationModal
+      isOpen={isOpen}
+      onOpenChange={onOpenChange}
+      title='Unlink QuickBooks'
+      description='Please confirm that you want to unlink QuickBooks.'
+      onConfirm={unlinkQuickbooks}
+      confirmLabel='Unlink QuickBooks'
+      errorText='Unlink failed. Please check your connection and try again in a few seconds.'
+    />
   )
 }
