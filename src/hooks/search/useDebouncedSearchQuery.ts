90a53314256f85b975ee651855df7799735a0e88
Kesin Ryan Dehejia <kesin@layerfi.com>
2025-06-16 16:34:27 -0700

    cr: fix `SecondPartySelector`
    - prefer single network request for dealing with metadata

---
+++ src/hooks/search/useDebouncedSearchQuery.ts
@@ -0,0 +1,45 @@
+import { useState, useCallback } from 'react'
+import { useDebounce } from '../useDebounce/useDebounce'
+
+type UseDebouncedSearchQueryOptions = {
+  initialInputState: string | (() => string)
+}
+
+export function useDebouncedSearchInput({
+  initialInputState,
+}: UseDebouncedSearchQueryOptions) {
+  const [inputValue, setInputValue] = useState(initialInputState)
+  const [searchQuery, setSearchQuery] = useState(() => inputValue)
+
+  const debouncedSetSearchQuery = useDebounce(
+    useCallback(
+      (value: string) => { setSearchQuery(value) },
+      [],
+    ),
+  )
+
+  const handleInputChange = useCallback(
+    (value: string) => {
+      setInputValue(value)
+
+      if (value === '') {
+        /*
+         * When the input is cleared, we want to clear the search query immediately.
+         */
+        debouncedSetSearchQuery.cancel()
+        setSearchQuery('')
+
+        return
+      }
+
+      debouncedSetSearchQuery(value)
+    },
+    [debouncedSetSearchQuery],
+  )
+
+  return {
+    inputValue,
+    searchQuery,
+    handleInputChange,
+  }
+}
