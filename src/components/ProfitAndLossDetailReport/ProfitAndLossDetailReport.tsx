1d11bd62e97afd8e93a3d69dc4546032404fe91d
Daniel O'Neel <danieloneel@gmail.com>
2025-08-22 14:37:42 -0700

    default to false and make optional

--- src/components/ProfitAndLossDetailReport/ProfitAndLossDetailReport.tsx
+++ src/components/ProfitAndLossDetailReport/ProfitAndLossDetailReport.tsx
@@ -234,6 +234,7 @@ export const ProfitAndLossDetailReport = ({
           data={rowsWithRunningBalance.lines}
           isLoading={isLoading}
           isError={isError}
+          shrinkHeightToFitRows={true}
           slots={{
             EmptyState,
             ErrorState,
