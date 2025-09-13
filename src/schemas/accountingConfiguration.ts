6cc45905e398c2dcba96185f1d2f3fef1cccc777
Shyam Paidipati <shyam@layerfi.com>
2025-09-12 19:05:48 -0700

    Fix account numbers

--- src/schemas/accountingConfiguration.ts
+++ src/schemas/accountingConfiguration.ts
@@ -16,26 +16,7 @@ export const CategoryListModeSchema = Schema.Enums(AccountingConfigurationCatego
 
 export const AccountingConfigurationSchema = Schema.Struct({
   id: Schema.UUID,
-  defaultReportingBasis: pipe(
-    Schema.propertySignature(Schema.NullOr(ReportingBasisSchema)),
-    Schema.fromKey('default_reporting_basis'),
-  ),
-  categoryListMode: pipe(
-    Schema.propertySignature(Schema.NullOr(CategoryListModeSchema)),
-    Schema.fromKey('category_list_mode'),
-  ),
-  matchRoundingThreshold: pipe(
-    Schema.propertySignature(Schema.NullOr(Schema.Number)),
-    Schema.fromKey('match_rounding_threshold'),
-  ),
-  matchAdjustmentThreshold: pipe(
-    Schema.propertySignature(Schema.NullOr(Schema.Number)),
-    Schema.fromKey('match_adjustment_threshold'),
-  ),
-  disableAdjustmentMatchSuggestions: pipe(
-    Schema.propertySignature(Schema.NullOr(Schema.Boolean)),
-    Schema.fromKey('disable_adjustment_match_suggestions'),
-  ),
+
   enableAccountNumbers: pipe(
     Schema.propertySignature(Schema.Boolean),
     Schema.fromKey('enable_account_numbers'),
