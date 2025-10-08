5b9ec61779914e8f66064b7d4c52b6d4bffd52d5
Jim Wei <105657653+jimwei1@users.noreply.github.com>
2025-10-08 12:18:07 -0700

    Fix eslint error

--- src/contexts/BankTransactionsFiltersContext/BankTransactionsFiltersProvider.tsx
+++ src/contexts/BankTransactionsFiltersContext/BankTransactionsFiltersProvider.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from 'react'
 import {
   BankTransactionsFiltersContext,
-  BankTransactionsFiltersContextType,
 } from './BankTransactionsFiltersContext'
 import { useBankTransactionsFilters, useBankTransactionsFiltersParams } from './useBankTransactionsFilters'
 
