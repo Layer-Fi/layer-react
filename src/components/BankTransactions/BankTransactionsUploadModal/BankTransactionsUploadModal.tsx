cd4a84a718410ffada328040dbc2fcb3b371bcfc
Darren <darren.sapalo@gmail.com>
2025-10-17 15:38:50 -0700

    Deletions

--- src/components/BankTransactions/BankTransactionsUploadModal/BankTransactionsUploadModal.tsx
+++ src/components/BankTransactions/BankTransactionsUploadModal/BankTransactionsUploadModal.tsx
@@ -1,16 +1,12 @@
 import { Modal, ModalProps } from '../../ui/Modal/Modal'
 import { UploadTransactions } from '../../UploadTransactions/UploadTransactions'
 
-function BankTransactionsUploadModalContent({ onClose }: { onClose: () => void }) {
-  return <UploadTransactions onComplete={onClose} />
-}
-
 type BankTransactionsUploadModalProps = Pick<ModalProps, 'isOpen' | 'onOpenChange'>
 export function BankTransactionsUploadModal({ isOpen, onOpenChange }: BankTransactionsUploadModalProps) {
   return (
     <Modal flexBlock isOpen={isOpen} onOpenChange={onOpenChange} size='lg' aria-label='Upload transactions'>
       {({ close }) => {
-        return <BankTransactionsUploadModalContent onClose={close} />
+        return <UploadTransactions onComplete={close} />
       }}
     </Modal>
   )
