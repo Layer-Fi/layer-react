42a4ed5fa3010d4db0c14ea44ee3fd359519e76e
Jim Wei <105657653+jimwei1@users.noreply.github.com>
2025-10-23 15:01:48 -0700

    Add RecategorizeAll, UncategorizeAll buttons to "Categorized" view, integrate Uncategorize All

--- src/components/BankTransactions/BankTransactionsBulkActions/BankTransactionsBulkActions.tsx
+++ src/components/BankTransactions/BankTransactionsBulkActions/BankTransactionsBulkActions.tsx
@@ -1,12 +1,29 @@
 import { HStack } from '../../ui/Stack/Stack'
 import { BankTransactionsConfirmAllButton } from './BankTransactionsConfirmAllButton'
 import { BankTransactionsCategorizeAllButton } from './BankTransactionsCategorizeAllButton'
+import { BankTransactionsRecategorizeAllButton } from './BankTransactionsRecategorizeAllButton'
+import { BankTransactionsUncategorizeAllButton } from './BankTransactionsUncategorizeAllButton'
+import { useBankTransactionsContext } from '../../../contexts/BankTransactionsContext'
+import { DisplayState } from '../../../types/bank_transactions'
 
 export const BankTransactionsBulkActions = () => {
+  const { display } = useBankTransactionsContext()
+
   return (
     <HStack pis='3xl' align='center' gap='sm'>
-      <BankTransactionsCategorizeAllButton />
-      <BankTransactionsConfirmAllButton />
+      {display === DisplayState.review
+        ? (
+          <>
+            <BankTransactionsCategorizeAllButton />
+            <BankTransactionsConfirmAllButton />
+          </>
+        )
+        : (
+          <>
+            <BankTransactionsRecategorizeAllButton />
+            <BankTransactionsUncategorizeAllButton />
+          </>
+        )}
     </HStack>
   )
 }
