e5d8ba7f30d11271163e5101bf546ab9b1e04a15
Jim Wei <jim@layerfi.com>
2025-10-30 15:01:15 -0700

    Move helper to useSaveBankTransactionRow hook

--- src/hooks/useBankTransactions/useSaveBankTransactionRow.ts
+++ src/hooks/useBankTransactions/useSaveBankTransactionRow.ts
@@ -2,8 +2,25 @@ import { BankTransaction } from '../../types/bank_transactions'
 import { BankTransactionCategoryComboBoxOption } from '../../components/BankTransactionCategoryComboBox/bankTransactionCategoryComboBoxOption'
 import { isPlaceholderAsOption, isSuggestedMatchAsOption, isSplitAsOption } from '../../components/BankTransactionCategoryComboBox/bankTransactionCategoryComboBoxOption'
 import { useCallback, useMemo } from 'react'
-import { buildSplitCategorizationRequest } from '../../components/BankTransactionRow/utils'
 import { useBankTransactionsContext } from '../../contexts/BankTransactionsContext'
+import { CategoryUpdate } from '../../types/categories'
+import { ClassificationEncoded } from '../../schemas/categorization'
+import { makeTagKeyValueFromTag } from '../../features/tags/tagSchemas'
+import { type Split } from '../../types/bank_transactions'
+import { SplitAsOption } from '../../types/categorizationOption'
+
+const buildSplitCategorizationRequest = (selectedCategory: SplitAsOption): CategoryUpdate => {
+  return {
+    type: 'Split',
+    entries: selectedCategory.original.map((split: Split) => ({
+      category: split.category?.classificationEncoded as ClassificationEncoded,
+      amount: split.amount,
+      tags: split.tags.map(tag => makeTagKeyValueFromTag(tag)),
+      customer_id: split.customerVendor?.customerVendorType === 'CUSTOMER' ? split.customerVendor.id : null,
+      vendor_id: split.customerVendor?.customerVendorType === 'VENDOR' ? split.customerVendor.id : null,
+    })),
+  }
+}
 
 type UseSaveBankTransactionRowResult = {
   saveBankTransactionRow: (selectedCategory: BankTransactionCategoryComboBoxOption | null | undefined, bankTransaction: BankTransaction) => Promise<void>
