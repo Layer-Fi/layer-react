58164104ab7ddd24a089d4f167d801b796ee89a3
Sarah Raines <sarah@layerfi.com>
2025-11-05 14:28:02 -0800

    Revert all changes to `Invoices`

--- src/components/Invoices/Invoices.tsx
+++ src/components/Invoices/Invoices.tsx
@@ -9,12 +9,6 @@ import { usePreloadTagDimensionByKey } from '../../features/tags/api/useTagDimen
 import { INVOICE_MECE_TAG_DIMENSION } from './InvoiceForm/formUtils'
 import type { Awaitable } from '../../types/utility/promises'
 import { InvoicesProvider } from '../../contexts/InvoicesContext/InvoicesContext'
-import { useGlobalDateRange, useGlobalDateRangeActions } from '../../providers/GlobalDateStore/GlobalDateStoreProvider'
-import { useCallback } from 'react'
-import { getLocalTimeZone, ZonedDateTime, fromDate } from '@internationalized/date'
-import { UnifiedReport } from '../UnifiedReport/UnifiedReport'
-import { GlobalMonthPicker } from '../GlobalMonthPicker/GlobalMonthPicker'
-
 interface InvoicesStringOverrides {
   title?: string
 }
@@ -29,20 +23,11 @@ export const Invoices = ({ showTitle = true, stringOverrides, onSendInvoice }: I
   usePreloadCustomers()
   usePreloadCategories({ mode: CategoriesListMode.Revenue })
   usePreloadTagDimensionByKey({ dimensionKey: INVOICE_MECE_TAG_DIMENSION })
-  const { startDate } = useGlobalDateRange({ displayMode: 'monthPicker' })
-  const { setMonth } = useGlobalDateRangeActions()
-
-  const passedDate = fromDate(startDate, getLocalTimeZone())
-  const setReceivedDate = useCallback((val: ZonedDateTime | null) => {
-    if (val == null) return
-    setMonth({ startDate: val.toDate() })
-  }, [setMonth])
 
   return (
     <InvoicesProvider onSendInvoice={onSendInvoice}>
       <View title={stringOverrides?.title || 'Invoices'} showHeader={showTitle}>
-        <GlobalMonthPicker />
-        <UnifiedReport />
+        <InvoicesContent />
       </View>
     </InvoicesProvider>
   )
