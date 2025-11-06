411f2037203f7558ad4e1930495d8d0d32b7c9e4
Jim Wei <jim@layerfi.com>
2025-11-05 16:07:42 -0800

    Update BankTransactionsCategorizedSelectedValue.tsx

--- src/components/BankTransactionsSelectedValue/BankTransactionsCategorizedSelectedValue.tsx
+++ src/components/BankTransactionsSelectedValue/BankTransactionsCategorizedSelectedValue.tsx
@@ -5,10 +5,11 @@ import { CategorizationStatus } from '../../schemas/bankTransactions/bankTransac
 import { isTransferMatch } from '../../utils/bankTransactions'
 import { Badge } from '../Badge'
 import { Span } from '../ui/Typography/Text'
-import { MatchBadge } from '../BankTransactionRow/MatchBadge'
-import { SplitTooltipDetails } from '../BankTransactionRow/SplitTooltipDetails'
 import { extractDescriptionForSplit } from '../BankTransactionRow/BankTransactionRow'
 import { DATE_FORMAT } from '../../config/general'
+import { HStack } from '../ui/Stack/Stack'
+import { BadgeSize } from '../Badge/Badge'
+import MinimizeTwo from '../../icons/MinimizeTwo'
 
 type BankTransactionsCategorizedSelectedValueProps = {
   bankTransaction: BankTransaction
@@ -19,49 +20,34 @@ export const BankTransactionsCategorizedSelectedValue = ({
   bankTransaction,
   className,
 }: BankTransactionsCategorizedSelectedValueProps) => {
-  return (
-    <Span ellipsis size='sm' className={className}>
-      {bankTransaction.categorization_status === CategorizationStatus.SPLIT && (
-        <>
-          <Badge
-            icon={<Scissors size={11} />}
-            tooltip={(
-              <SplitTooltipDetails
-                classNamePrefix='Layer__bank-transaction-row'
-                category={bankTransaction.category}
-              />
-            )}
-          >
-            Split
-          </Badge>
-          <Span ellipsis>
-            {extractDescriptionForSplit(bankTransaction.category)}
-          </Span>
-        </>
-      )}
-      {bankTransaction?.categorization_status === CategorizationStatus.MATCHED
-        && bankTransaction?.match && (
-        <>
-          <MatchBadge
-            classNamePrefix='Layer__bank-transaction-row'
-            bankTransaction={bankTransaction}
-            dateFormat={DATE_FORMAT}
-            text={isTransferMatch(bankTransaction) ? 'Transfer' : 'Match'}
-          />
-          <Span ellipsis>
-            {`${formatTime(
-              parseISO(bankTransaction.match.bank_transaction.date),
-              DATE_FORMAT,
-            )}, ${bankTransaction.match?.details?.description}`}
-          </Span>
-        </>
-      )}
-      {bankTransaction?.categorization_status !== CategorizationStatus.MATCHED
-        && bankTransaction?.categorization_status !== CategorizationStatus.SPLIT && (
-        <Span ellipsis>
-          {bankTransaction?.category?.display_name}
+  if (bankTransaction.categorization_status === CategorizationStatus.MATCHED && bankTransaction.match) {
+    return (
+      <HStack gap='xs' align='center' className={className}>
+        <Badge size={BadgeSize.SMALL} icon={<MinimizeTwo size={11} />}>
+          {isTransferMatch(bankTransaction) ? 'Transfer' : 'Match'}
+        </Badge>
+        <Span ellipsis size='sm'>
+          {`${formatTime(
+            parseISO(bankTransaction.match.bank_transaction.date),
+            DATE_FORMAT,
+          )}, ${bankTransaction.match?.details?.description}`}
         </Span>
-      )}
-    </Span>
-  )
+      </HStack>
+    )
+  }
+
+  if (bankTransaction.categorization_status === CategorizationStatus.SPLIT) {
+    return (
+      <HStack gap='xs' align='center' className={className}>
+        <Badge size={BadgeSize.SMALL} icon={<Scissors size={11} />}>
+          Split
+        </Badge>
+        <Span ellipsis size='sm'>
+          {extractDescriptionForSplit(bankTransaction.category)}
+        </Span>
+      </HStack>
+    )
+  }
+
+  return <Span ellipsis size='sm' className={className}>{bankTransaction.category?.display_name}</Span>
 }
