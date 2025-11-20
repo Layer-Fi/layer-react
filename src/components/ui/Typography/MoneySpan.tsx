458dd900a45ff8ee65b2f71fa54942a3ffaeb8db
Sarah Raines <sarah@layerfi.com>
2025-11-19 17:24:36 -0800

    Actually use the `Span` component under `MoneySpan`

--- src/components/ui/Typography/MoneySpan.tsx
+++ src/components/ui/Typography/MoneySpan.tsx
@@ -3,11 +3,11 @@ import classNames from 'classnames'
 
 import { centsToDollars as formatMoney } from '@models/Money'
 import { toDataProperties } from '@utils/styleUtils/toDataProperties'
-import type { TextStyleProps } from '@ui/Typography/Text'
+import { Span, type TextStyleProps } from '@ui/Typography/Text'
 
 import './moneySpan.scss'
 
-const CLASS_NAME = 'Layer__MoneySpan Layer__Span'
+const CLASS_NAME = 'Layer__MoneySpan'
 
 type MoneySpanProps = {
   amount: number
@@ -15,42 +15,17 @@ type MoneySpanProps = {
 } & TextStyleProps & Pick<ComponentPropsWithoutRef<'span'>, 'slot'>
 
 const MoneySpan = forwardRef<HTMLSpanElement, MoneySpanProps>(
-  ({
-    amount,
-    displayPlusSign,
-    align,
-    ellipsis,
-    noWrap,
-    pb,
-    pbe,
-    pbs,
-    size,
-    status,
-    variant,
-    weight,
-    className,
-    ...restProps
-  }, ref) => {
+  ({ amount, displayPlusSign, className, ...restProps }, ref) => {
     const dataProperties = toDataProperties({
       'positive': amount >= 0,
       'negative': amount < 0,
       'display-plus-sign': displayPlusSign,
-      align,
-      ellipsis,
-      'no-wrap': noWrap,
-      pb,
-      pbe,
-      pbs,
-      size,
-      status,
-      variant,
-      weight,
     })
 
     return (
-      <span {...restProps} {...dataProperties} className={classNames(CLASS_NAME, className)} ref={ref}>
+      <Span {...restProps} {...dataProperties} className={classNames(CLASS_NAME, className)} ref={ref}>
         {formatMoney(Math.abs(amount))}
-      </span>
+      </Span>
     )
   },
 )
