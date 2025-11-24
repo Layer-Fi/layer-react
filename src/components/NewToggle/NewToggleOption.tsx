6ca5233cfad07cad2ddb7c58b96c938cce52e7fa
Jim Wei <jim@layerfi.com>
2025-11-24 14:00:17 -0800

    Update NewToggleOption.tsx

--- src/components/NewToggle/NewToggleOption.tsx
+++ src/components/NewToggle/NewToggleOption.tsx
@@ -1,4 +1,3 @@
-import type { CSSProperties } from 'react'
 import { SelectionIndicator, ToggleButton } from 'react-aria-components'
 
 import { Span } from '@ui/Typography/Text'
@@ -7,16 +6,14 @@ import './newToggleOption.scss'
 export interface NewToggleOptionProps {
   label: string
   value: string
-  style?: CSSProperties
 }
 
 export const NewToggleOption = ({
   label,
   value,
-  style,
 }: NewToggleOptionProps) => {
   return (
-    <ToggleButton id={value} style={style} className='Layer__NewToggleOption'>
+    <ToggleButton id={value} className='Layer__NewToggleOption'>
       <SelectionIndicator className='Layer__NewToggleOption-SelectionIndicator' />
       <Span className='Layer__NewToggle-Option-Content'>
         <Span noWrap>{label}</Span>
