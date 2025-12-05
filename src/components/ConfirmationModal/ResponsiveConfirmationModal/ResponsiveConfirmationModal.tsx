ad118337faf2445977cb272b01a27419c17f59e1
Jim Wei <jim@layerfi.com>
2025-12-05 14:51:46 -0800

    Add ResponsiveConfirmationModal.tsx

---
+++ src/components/ConfirmationModal/ResponsiveConfirmationModal/ResponsiveConfirmationModal.tsx
@@ -0,0 +1,128 @@
+import { type ReactNode, useCallback, useState } from 'react'
+
+import { type Awaitable } from '@internal-types/utility/promises'
+import { APIError } from '@models/APIError'
+import { Drawer, Modal, type ModalProps } from '@ui/Modal/Modal'
+import {
+  ModalActions,
+  ModalContent,
+  ModalDescription,
+  ModalHeading,
+  ModalTitleWithClose,
+} from '@ui/Modal/ModalSlots'
+import { HStack, Spacer, VStack } from '@ui/Stack/Stack'
+import { Button, ButtonVariant } from '@components/Button/Button'
+import { SubmitButton } from '@components/Button/SubmitButton'
+
+export type ResponsiveConfirmationModalProps = Pick<ModalProps, 'isOpen' | 'onOpenChange'> & {
+  title: string
+  description?: string
+  content?: ReactNode
+  onConfirm: () => Awaitable<void>
+  confirmLabel?: string
+  retryLabel?: string
+  cancelLabel?: string
+  errorText?: string
+  closeOnConfirm?: boolean
+  confirmDisabled?: boolean
+  useDrawer?: boolean
+}
+
+export function ResponsiveConfirmationModal({
+  isOpen,
+  onOpenChange,
+  title,
+  description,
+  content,
+  onConfirm,
+  confirmLabel = 'Confirm',
+  retryLabel = 'Retry',
+  cancelLabel = 'Cancel',
+  errorText,
+  closeOnConfirm = true,
+  confirmDisabled = false,
+  useDrawer = false,
+}: ResponsiveConfirmationModalProps) {
+  const [isProcessing, setIsProcessing] = useState(false)
+  const [error, setError] = useState<APIError | Error | null>(null)
+
+  const onClickConfirm = useCallback((close: () => void) => {
+    setIsProcessing(true)
+    void Promise.resolve(onConfirm())
+      .then(() => {
+        if (closeOnConfirm) close()
+      })
+      .catch((e: APIError | Error) => {
+        setError(e)
+      })
+      .finally(() => {
+        setIsProcessing(false)
+      })
+  }, [closeOnConfirm, onConfirm])
+
+  const getErrorMessage = (error: APIError | Error | null, errorText?: string) => {
+    if (error === null) return null
+
+    if (errorText) return errorText
+
+    return error instanceof APIError
+      ? error?.getAllMessages()?.[0] || error?.getMessage()
+      : error?.message
+  }
+
+  const modalContent = (close: () => void) => (
+    <VStack>
+      <ModalTitleWithClose
+        heading={(
+          <ModalHeading size='sm'>
+            {title}
+          </ModalHeading>
+        )}
+        onClose={close}
+      />
+      <VStack gap='md'>
+        {description && <ModalDescription>{description}</ModalDescription>}
+        {content && <ModalContent>{content}</ModalContent>}
+      </VStack>
+      <ModalActions>
+        <HStack gap='md'>
+          <Spacer />
+          <Button variant={ButtonVariant.secondary} onClick={close}>
+            {cancelLabel}
+          </Button>
+          <SubmitButton
+            onClick={() => onClickConfirm(close)}
+            processing={isProcessing}
+            disabled={confirmDisabled}
+            error={getErrorMessage(error, errorText) ?? ''}
+            withRetry
+            noIcon={!isProcessing}
+          >
+            {error ? retryLabel : confirmLabel}
+          </SubmitButton>
+        </HStack>
+      </ModalActions>
+    </VStack>
+  )
+
+  if (useDrawer) {
+    return (
+      <Drawer
+        flexBlock
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        variant='mobile-drawer'
+        isDismissable
+        role='alertdialog'
+      >
+        {({ close }) => modalContent(close)}
+      </Drawer>
+    )
+  }
+
+  return (
+    <Modal flexBlock isOpen={isOpen} onOpenChange={onOpenChange} role='alertdialog'>
+      {({ close }) => modalContent(close)}
+    </Modal>
+  )
+}
