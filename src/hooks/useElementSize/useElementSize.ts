6f127dab08122eb28605252f20b18b2a4b4f4d50
Jim Wei <jim@layerfi.com>
2026-01-13 13:13:29 -0800

    Add immediate first callback on useElementSize

--- src/hooks/useElementSize/useElementSize.ts
+++ src/hooks/useElementSize/useElementSize.ts
@@ -14,6 +14,7 @@ export const useElementSize = <T extends HTMLElement>(
 ) => {
   const ref = useRef<T>(null)
   const resizeTimeout = useRef<number | null>(null)
+  const isFirstCallback = useRef(true)
 
   useLayoutEffect(() => {
     const element = ref?.current
@@ -22,18 +23,29 @@ export const useElementSize = <T extends HTMLElement>(
       return
     }
 
+    const invokeCallback = (entry: ResizeObserverEntry) => {
+      callback(element, entry, {
+        width: element.offsetWidth,
+        height: element.offsetHeight,
+        clientWidth: element.clientWidth,
+        clientHeight: element.clientHeight,
+      })
+    }
+
     const observer = new ResizeObserver((entries) => {
+      const entry = entries[0]
+
+      if (isFirstCallback.current) {
+        isFirstCallback.current = false
+        invokeCallback(entry)
+        return
+      }
+
       if (resizeTimeout.current) {
         clearTimeout(resizeTimeout.current)
       }
       resizeTimeout.current = window.setTimeout(() => {
-        const entry = entries[0]
-        callback(element, entry, {
-          width: element.offsetWidth,
-          height: element.offsetHeight,
-          clientWidth: element.clientWidth,
-          clientHeight: element.clientHeight,
-        })
+        invokeCallback(entry)
       }, 100)
     })
     observer.observe(element)
