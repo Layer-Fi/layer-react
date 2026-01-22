f73fe462df2df6f12633f314c5824370472c91c8
Sarah Raines <sarah@layerfi.com>
2026-01-22 13:24:28 -0800

    do not show fields inline unless desktop

--- src/components/TaxProfileForm/sections/DeductionsSection.tsx
+++ src/components/TaxProfileForm/sections/DeductionsSection.tsx
@@ -2,17 +2,13 @@ import { BigDecimal as BD } from 'effect'
 
 import { VStack } from '@ui/Stack/Stack'
 import { Heading } from '@ui/Typography/Heading'
-import { type TaxProfileForm } from '@components/TaxProfileForm/taxProfileFormSchema'
-import { type AppForm } from '@features/forms/hooks/useForm'
-
-type DeductionsSectionProps = {
-  form: AppForm<TaxProfileForm>
-  isReadOnly?: boolean
-}
+import { getFormFieldProps, type TaxProfileFormSectionProps } from '@components/TaxProfileForm/formUtils'
 
 const MAX_HOME_OFFICE_AREA = BD.fromBigInt(BigInt(300))
 
-export const DeductionsSection = ({ form, isReadOnly }: DeductionsSectionProps) => {
+export const DeductionsSection = ({ form, isReadOnly, isDesktop }: TaxProfileFormSectionProps) => {
+  const desktopFieldProps = getFormFieldProps(isDesktop)
+
   return (
     <VStack className='Layer__TaxProfileForm__Section' gap='md'>
       <Heading level={3}>Deductions</Heading>
@@ -20,10 +16,9 @@ export const DeductionsSection = ({ form, isReadOnly }: DeductionsSectionProps) 
       <form.AppField name='usConfiguration.deductions.homeOffice.useHomeOfficeDeduction'>
         {field => (
           <field.FormRadioGroupYesNoField
-            className='Layer__TaxProfileForm__Field'
             label='Use simplified home office deduction?'
             isReadOnly={isReadOnly}
-            inline
+            {...desktopFieldProps}
           />
         )}
       </form.AppField>
@@ -33,14 +28,13 @@ export const DeductionsSection = ({ form, isReadOnly }: DeductionsSectionProps) 
           <form.AppField name='usConfiguration.deductions.homeOffice.homeOfficeArea'>
             {field => (
               <field.FormBigDecimalField
-                className='Layer__TaxProfileForm__Field'
                 label='Home office area (sq ft)'
-                inline
                 isReadOnly={isReadOnly}
                 placeholder='Enter area'
                 mode='decimal'
                 maxDecimalPlaces={3}
                 maxValue={MAX_HOME_OFFICE_AREA}
+                {...desktopFieldProps}
               />
             )}
           </form.AppField>
@@ -50,10 +44,9 @@ export const DeductionsSection = ({ form, isReadOnly }: DeductionsSectionProps) 
       <form.AppField name='usConfiguration.deductions.vehicle.useMileageDeduction'>
         {field => (
           <field.FormRadioGroupYesNoField
-            className='Layer__TaxProfileForm__Field'
             label='Use standard mileage deduction?'
             isReadOnly={isReadOnly}
-            inline
+            {...desktopFieldProps}
           />
         )}
       </form.AppField>
