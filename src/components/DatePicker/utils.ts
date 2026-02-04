6320266e5a55659090304d902052c820f73d9670
Jim Wei <jim@layerfi.com>
2026-02-04 14:27:02 -0800

    Update utils.ts

--- src/components/DatePicker/utils.ts
+++ src/components/DatePicker/utils.ts
@@ -1,15 +1,12 @@
-import { type ZonedDateTime, DateFormatter } from '@internationalized/date'
+import type { ZonedDateTime } from '@internationalized/date'
 
-const formatDate = (date: ZonedDateTime): string => {
-  const formatter = new DateFormatter('en-US', { month: 'short', day: 'numeric', year: 'numeric' })
-  return formatter.format(date.toDate())
-}
+import { formatDate } from '@utils/format'
 
 export const getIsDateInvalid = (
   date: ZonedDateTime | null,
   { minDate, maxDate }: { minDate?: ZonedDateTime | null, maxDate?: ZonedDateTime | null }) => {
   if (date === null) return 'Cannot select empty date'
-  if (minDate && date.compare(minDate) < 0) return `Cannot select date before ${formatDate(minDate)}`
+  if (minDate && date.compare(minDate) < 0) return `Cannot select date before ${formatDate(minDate.toDate())}`
   if (maxDate && date.compare(maxDate) > 0) return 'Cannot select date in the future'
   return null
 }
