4fa085754bc6aebafffabc8d5abc792dfd2c4a02
Jim Wei <jim@layerfi.com>
2026-02-04 14:19:26 -0800

    Show {activation_date} instead of `activation_date` on tootlip hover

--- src/components/DatePicker/utils.ts
+++ src/components/DatePicker/utils.ts
@@ -1,10 +1,15 @@
-import { type ZonedDateTime } from '@internationalized/date'
+import { type ZonedDateTime, DateFormatter } from '@internationalized/date'
+
+const formatDate = (date: ZonedDateTime): string => {
+  const formatter = new DateFormatter('en-US', { month: 'short', day: 'numeric', year: 'numeric' })
+  return formatter.format(date.toDate())
+}
 
 export const getIsDateInvalid = (
   date: ZonedDateTime | null,
   { minDate, maxDate }: { minDate?: ZonedDateTime | null, maxDate?: ZonedDateTime | null }) => {
   if (date === null) return 'Cannot select empty date'
-  if (minDate && date.compare(minDate) < 0) return 'Cannot select date before the business activation date'
+  if (minDate && date.compare(minDate) < 0) return `Cannot select date before ${formatDate(minDate)}`
   if (maxDate && date.compare(maxDate) > 0) return 'Cannot select date in the future'
   return null
 }
