42bad2643bb33ff297fbdf89cb43c9a134cef6ad
Andrew Acomb <acombandrew@gmail.com>
2026-03-05 13:28:39 -0800

    rm bills

--- src/utils/vendors.ts
+++
@@ -1,5 +0,0 @@
-import { type Vendor } from '@internal-types/vendors'
-
-export const getVendorName = (vendor?: Vendor): string => (
-  vendor?.individual_name ?? vendor?.company_name ?? vendor?.external_id ?? ''
-)
