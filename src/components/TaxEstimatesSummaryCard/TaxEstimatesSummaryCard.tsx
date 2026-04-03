ae2f74ae076abb8690fe33aed7299628845f6949
Darren <darren.sapalo@gmail.com>
2026-04-03 09:28:15 -0700

    refactor: use container queries for tax estimates summary card styles

--- src/components/TaxEstimatesSummaryCard/TaxEstimatesSummaryCard.tsx
+++ src/components/TaxEstimatesSummaryCard/TaxEstimatesSummaryCard.tsx
@@ -25,36 +25,38 @@ export const TaxEstimatesSummaryCard = ({
   const isSummaryCardLayout = layout === 'summaryCard'
 
   return (
-    <Card className={classNames('Layer__TaxEstimatesSummaryCard', isSummaryCardLayout && 'Layer__TaxEstimatesSummaryCard--summaryCard', className)}>
-      <VStack gap='md' className='Layer__TaxEstimatesSummaryCard__Body'>
-        <HStack
-          className={classNames('Layer__TaxEstimatesSummaryCard__Header', isSummaryCardLayout && 'Layer__SummaryCard__ContainerHeader')}
-          justify='space-between'
-          align={isSummaryCardLayout ? 'center' : 'start'}
-          gap='md'
-        >
-          <Span size='lg' weight='bold'>{title}</Span>
-          {headerAction}
-        </HStack>
-        {(isMobile || isSummaryCardLayout)
-          ? (
-            <VStack className='Layer__TaxEstimatesSummaryCard__Content Layer__TaxEstimatesSummaryCard__Content--mobile' gap='lg'>
-              <DonutChart categories={categories} total={total} />
-              <Legend categories={categories} total={total} isMobile={isMobile} />
-            </VStack>
-          )
-          : (
-            <HStack className='Layer__TaxEstimatesSummaryCard__Content' align='center' gap='lg'>
-              <DonutChart categories={categories} total={total} />
-              <Legend categories={categories} total={total} isMobile={false} />
-            </HStack>
-          )}
-        <NextPaymentBanner
-          isSummaryCardLayout={isSummaryCardLayout}
-          isMobile={isMobile}
-          nextTax={nextTax}
-        />
-      </VStack>
-    </Card>
+    <VStack className='Layer__TaxEstimatesSummaryCard__Container'>
+      <Card className={classNames('Layer__TaxEstimatesSummaryCard', isSummaryCardLayout && 'Layer__TaxEstimatesSummaryCard--summaryCard', className)}>
+        <VStack gap='md' className='Layer__TaxEstimatesSummaryCard__Body'>
+          <HStack
+            className={classNames('Layer__TaxEstimatesSummaryCard__Header', isSummaryCardLayout && 'Layer__SummaryCard__ContainerHeader')}
+            justify='space-between'
+            align={isSummaryCardLayout ? 'center' : 'start'}
+            gap='md'
+          >
+            <Span size='lg' weight='bold'>{title}</Span>
+            {headerAction}
+          </HStack>
+          {(isMobile || isSummaryCardLayout)
+            ? (
+              <VStack className='Layer__TaxEstimatesSummaryCard__Content Layer__TaxEstimatesSummaryCard__Content--mobile' gap='lg'>
+                <DonutChart categories={categories} total={total} />
+                <Legend categories={categories} total={total} isMobile={isMobile} />
+              </VStack>
+            )
+            : (
+              <HStack className='Layer__TaxEstimatesSummaryCard__Content' align='center' gap='lg'>
+                <DonutChart categories={categories} total={total} />
+                <Legend categories={categories} total={total} isMobile={false} />
+              </HStack>
+            )}
+          <NextPaymentBanner
+            isSummaryCardLayout={isSummaryCardLayout}
+            isMobile={isMobile}
+            nextTax={nextTax}
+          />
+        </VStack>
+      </Card>
+    </VStack>
   )
 }
