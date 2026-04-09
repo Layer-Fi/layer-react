2f240d5b1bf8cca25789b2e35ac10c9dcfb8978a
Darren <darren.sapalo@gmail.com>
2026-04-09 11:26:56 -0700

    removals

--- src/views/TaxEstimates/TaxEstimatesOnboardedViewContent.tsx
+++ src/views/TaxEstimates/TaxEstimatesOnboardedViewContent.tsx
@@ -2,12 +2,10 @@ import { useCallback, useMemo } from 'react'
 import type { Key } from 'react-aria-components'
 import { useTranslation } from 'react-i18next'
 
-import { type TaxEstimatesBanner } from '@schemas/taxEstimates/banner'
 import { translationKey } from '@utils/i18n/translationKey'
 import { TaxEstimatesRoute, useTaxEstimatesNavigation, useTaxEstimatesRouteState } from '@providers/TaxEstimatesRouteStore/TaxEstimatesRouteStoreProvider'
 import { VStack } from '@ui/Stack/Stack'
 import { Toggle } from '@ui/Toggle/Toggle'
-import { type TaxBannerReviewPayload } from '@components/TaxDetails/TaxBanner'
 import { TaxDetails } from '@components/TaxDetails/TaxDetails'
 import { TaxPayments } from '@components/TaxPayments/TaxPayments'
 import { TaxProfile } from '@views/TaxEstimates/TaxProfile'
@@ -18,18 +16,6 @@ const TAX_ESTIMATES_TAB_CONFIG = [
   { value: TaxEstimatesRoute.Estimates, ...translationKey('taxEstimates:label.estimates', 'Estimates') },
   { value: TaxEstimatesRoute.Payments, ...translationKey('taxEstimates:label.payments', 'Payments') },
 ]
-
-const getTaxBannerReviewPayload = (taxBanner?: TaxEstimatesBanner): TaxBannerReviewPayload | undefined => {
-  if (!taxBanner || taxBanner.totalUncategorizedCount <= 0) {
-    return
-  }
-
-  return {
-    type: 'UNCATEGORIZED_TRANSACTIONS',
-    count: taxBanner.totalUncategorizedCount,
-    amount: taxBanner.totalUncategorizedSum,
-  }
-}
 
 export const TaxEstimatesOnboardedViewContent = () => {
   const { t } = useTranslation()
