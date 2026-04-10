e03bdedf8db9f0b837cf00565078a576cd33d699
Darren <darren.sapalo@gmail.com>
2026-04-09 18:30:43 -0700

    fix: this prop will be filled in later by the banner PR

--- src/views/TaxEstimates/taxEstimatesTypes.ts
+++ src/views/TaxEstimates/taxEstimatesTypes.ts
@@ -1,5 +1,1 @@
-import type { TaxBannerReviewPayload } from '@components/TaxDetails/TaxBanner'
-
-export type TaxEstimatesViewProps = {
-  onTaxBannerReviewClick?: (payload: TaxBannerReviewPayload) => void
-}
+export type TaxEstimatesViewProps = object
