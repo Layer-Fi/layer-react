17a37f98d1766db024ed3337dba9c55c295d38cf
Darren <darren.sapalo@gmail.com>
2026-04-10 13:47:53 -0700

    no relative imports.

--- src/views/TaxEstimates/TaxEstimates.tsx
+++ src/views/TaxEstimates/TaxEstimates.tsx
@@ -4,13 +4,11 @@ import { useTranslation } from 'react-i18next'
 import { OnboardingStatus, TaxEstimatesRouteStoreProvider, useTaxEstimatesOnboardingStatus } from '@providers/TaxEstimatesRouteStore/TaxEstimatesRouteStoreProvider'
 import { View } from '@components/View/View'
 import { TaxEstimatesViewContent } from '@views/TaxEstimates/TaxEstimatesViewContent'
-
-import { TaxEstimatesViewHeader } from './TaxEstimatesViewHeader'
+import { TaxEstimatesViewHeader } from '@views/TaxEstimates/TaxEstimatesViewHeader'
 
 export const TaxEstimates = () => {
   return (
     <TaxEstimatesRouteStoreProvider>
-
       <TaxEstimatesView />
     </TaxEstimatesRouteStoreProvider>
   )
