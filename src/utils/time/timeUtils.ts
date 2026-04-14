f68162c39b6a08023a560261f21bb6ee4e4af382
Taylor Bustamante <taylor@layerfi.com>
2026-04-14 11:45:22 -0700

    Catching negative handling bug

--- src/utils/time/timeUtils.ts
+++ src/utils/time/timeUtils.ts
@@ -30,8 +30,8 @@ export const formatMinutesAsDuration = (
   totalMinutes: number,
   { compact = false }: { compact?: boolean } = {},
 ): string => {
-  const hours = Math.floor(totalMinutes / 60)
-  const minutes = totalMinutes % 60
+  const hours = Math.trunc(totalMinutes / 60)
+  const minutes = totalMinutes - hours * 60
 
   if (compact) {
     return `${String(hours).padStart(2, '0')}h${String(minutes).padStart(2, '0')}`
