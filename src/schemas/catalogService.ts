48d660cd644ca4f2f1c97b6114712e5be3967a89
Taylor Bustamante <taylor@layerfi.com>
2026-04-15 12:17:39 -0700

    Updating to optional on create

--- src/schemas/catalogService.ts
+++ src/schemas/catalogService.ts
@@ -22,13 +22,11 @@ export type CatalogService = typeof CatalogServiceSchema.Type
 export const CreateCatalogServiceSchema = Schema.Struct({
   name: Schema.String,
 
-  externalId: pipe(
-    Schema.propertySignature(Schema.NullishOr(Schema.String)),
+  externalId: Schema.optional(Schema.String).pipe(
     Schema.fromKey('external_id'),
   ),
 
-  accountIdentifier: pipe(
-    Schema.propertySignature(Schema.NullishOr(AccountIdentifierSchema)),
+  accountIdentifier: Schema.optional(AccountIdentifierSchema).pipe(
     Schema.fromKey('account_identifier'),
   ),
 
@@ -36,9 +34,9 @@ export const CreateCatalogServiceSchema = Schema.Struct({
     Schema.fromKey('billable_rate_per_hour_amount'),
   ),
 
-  memo: Schema.NullishOr(Schema.String),
+  memo: Schema.optional(Schema.String),
 
-  metadata: Schema.NullishOr(Schema.Unknown),
+  metadata: Schema.optional(Schema.Unknown),
 })
 export type CreateCatalogService = typeof CreateCatalogServiceSchema.Type
 export type CreateCatalogServiceEncoded = typeof CreateCatalogServiceSchema.Encoded
@@ -46,24 +44,21 @@ export type CreateCatalogServiceEncoded = typeof CreateCatalogServiceSchema.Enco
 export const UpdateCatalogServiceSchema = Schema.Struct({
   name: Schema.optional(Schema.String),
 
-  externalId: pipe(
-    Schema.propertySignature(Schema.NullishOr(Schema.String)),
+  externalId: Schema.optional(Schema.String).pipe(
     Schema.fromKey('external_id'),
   ),
 
-  accountIdentifier: pipe(
-    Schema.propertySignature(Schema.NullishOr(AccountIdentifierSchema)),
+  accountIdentifier: Schema.optional(AccountIdentifierSchema).pipe(
     Schema.fromKey('account_identifier'),
   ),
 
-  billableRatePerHourAmount: pipe(
-    Schema.propertySignature(Schema.NullishOr(Schema.Number)),
+  billableRatePerHourAmount: Schema.optional(Schema.Number).pipe(
     Schema.fromKey('billable_rate_per_hour_amount'),
   ),
 
-  memo: Schema.NullishOr(Schema.String),
+  memo: Schema.optional(Schema.String),
 
-  metadata: Schema.NullishOr(Schema.Unknown),
+  metadata: Schema.optional(Schema.Unknown),
 })
 export type UpdateCatalogService = typeof UpdateCatalogServiceSchema.Type
 export type UpdateCatalogServiceEncoded = typeof UpdateCatalogServiceSchema.Encoded
