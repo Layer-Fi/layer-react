bfc068071cfe5e7f26cd8a2ef328ce27ba808830
Taylor Bustamante <taylor@layerfi.com>
2026-04-15 11:48:04 -0700

    nullish or and account identifier schema

--- src/schemas/catalogService.ts
+++ src/schemas/catalogService.ts
@@ -1,5 +1,7 @@
 import { pipe, Schema } from 'effect'
 
+import { AccountIdentifierSchema } from '@schemas/accountIdentifier'
+
 export const CatalogServiceSchema = Schema.Struct({
   id: Schema.UUID,
 
@@ -10,7 +12,8 @@ export const CatalogServiceSchema = Schema.Struct({
     Schema.fromKey('billable_rate_per_hour_amount'),
   ),
 
-  archivedAt: Schema.NullishOr(Schema.Date).pipe(
+  archivedAt: pipe(
+    Schema.propertySignature(Schema.NullishOr(Schema.Date)),
     Schema.fromKey('archived_at'),
   ),
 })
@@ -19,11 +22,13 @@ export type CatalogService = typeof CatalogServiceSchema.Type
 export const CreateCatalogServiceSchema = Schema.Struct({
   name: Schema.String,
 
-  externalId: Schema.optional(Schema.NullOr(Schema.String)).pipe(
+  externalId: pipe(
+    Schema.propertySignature(Schema.NullishOr(Schema.String)),
     Schema.fromKey('external_id'),
   ),
 
-  accountIdentifier: Schema.optional(Schema.NullOr(Schema.String)).pipe(
+  accountIdentifier: pipe(
+    Schema.propertySignature(Schema.NullishOr(AccountIdentifierSchema)),
     Schema.fromKey('account_identifier'),
   ),
 
@@ -31,9 +36,9 @@ export const CreateCatalogServiceSchema = Schema.Struct({
     Schema.fromKey('billable_rate_per_hour_amount'),
   ),
 
-  memo: Schema.optional(Schema.NullOr(Schema.String)),
+  memo: Schema.NullishOr(Schema.String),
 
-  metadata: Schema.optional(Schema.NullOr(Schema.Unknown)),
+  metadata: Schema.NullishOr(Schema.Unknown),
 })
 export type CreateCatalogService = typeof CreateCatalogServiceSchema.Type
 export type CreateCatalogServiceEncoded = typeof CreateCatalogServiceSchema.Encoded
@@ -41,21 +46,24 @@ export type CreateCatalogServiceEncoded = typeof CreateCatalogServiceSchema.Enco
 export const UpdateCatalogServiceSchema = Schema.Struct({
   name: Schema.optional(Schema.String),
 
-  externalId: Schema.optional(Schema.NullOr(Schema.String)).pipe(
+  externalId: pipe(
+    Schema.propertySignature(Schema.NullishOr(Schema.String)),
     Schema.fromKey('external_id'),
   ),
 
-  accountIdentifier: Schema.optional(Schema.NullOr(Schema.String)).pipe(
+  accountIdentifier: pipe(
+    Schema.propertySignature(Schema.NullishOr(AccountIdentifierSchema)),
     Schema.fromKey('account_identifier'),
   ),
 
-  billableRatePerHourAmount: Schema.optional(Schema.NullOr(Schema.Number)).pipe(
+  billableRatePerHourAmount: pipe(
+    Schema.propertySignature(Schema.NullishOr(Schema.Number)),
     Schema.fromKey('billable_rate_per_hour_amount'),
   ),
 
-  memo: Schema.optional(Schema.NullOr(Schema.String)),
+  memo: Schema.NullishOr(Schema.String),
 
-  metadata: Schema.optional(Schema.NullOr(Schema.Unknown)),
+  metadata: Schema.NullishOr(Schema.Unknown),
 })
 export type UpdateCatalogService = typeof UpdateCatalogServiceSchema.Type
 export type UpdateCatalogServiceEncoded = typeof UpdateCatalogServiceSchema.Encoded
