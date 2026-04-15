ca88f74ab3e95e804d764fc77061994325d2dde0
Taylor Bustamante <taylor@layerfi.com>
2026-04-15 11:54:31 -0700

    Preserving size after locale changes

--- src/hooks/api/businesses/[business-id]/time-tracking/time-entries/useListTimeEntries.ts
+++ src/hooks/api/businesses/[business-id]/time-tracking/time-entries/useListTimeEntries.ts
@@ -10,6 +10,7 @@ import { toDefinedSearchParameters } from '@utils/request/toDefinedSearchParamet
 import { useLocalizedKey } from '@utils/swr/localeKeyMiddleware'
 import { SWRInfiniteResult } from '@utils/swr/SWRResponseTypes'
 import { useGlobalCacheActions } from '@utils/swr/useGlobalCacheActions'
+import { usePreserveInfiniteSize } from '@utils/swr/usePreserveInfiniteSize'
 import { useAuth } from '@hooks/utils/auth/useAuth'
 import { useLayerContext } from '@contexts/LayerContext/LayerContext'
 
@@ -170,6 +171,8 @@ export function useListTimeEntries(filterParams: ListTimeEntriesFilterParams = {
       initialSize: 1,
     },
   )
+
+  usePreserveInfiniteSize(swrResponse)
 
   return new SWRInfiniteResult(swrResponse)
 }
