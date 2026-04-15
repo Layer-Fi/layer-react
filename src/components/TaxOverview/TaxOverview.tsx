ae0267c82980a726c761e2a4569b6bea9b3ca5d3
Darren <darren.sapalo@gmail.com>
2026-04-15 10:55:48 -0700

    date formatting and DRY hooks for metric rows

--- src/components/TaxOverview/TaxOverview.tsx
+++ src/components/TaxOverview/TaxOverview.tsx
@@ -2,7 +2,9 @@ import { useTranslation } from 'react-i18next'
 
 import { type TaxOverviewApiData } from '@schemas/taxEstimates/overview'
 import { tConditional } from '@utils/i18n/conditional'
+import { DateFormat } from '@utils/i18n/date/patterns'
 import { useTaxOverview } from '@hooks/api/businesses/[business-id]/tax-estimates/overview/useTaxOverview'
+import { useIntlFormatter } from '@hooks/utils/i18n/useIntlFormatter'
 import { useSizeClass } from '@hooks/utils/size/useWindowSize'
 import { useFullYearProjection, useTaxEstimatesYear } from '@providers/TaxEstimatesRouteStore/TaxEstimatesRouteStoreProvider'
 import { VStack } from '@ui/Stack/Stack'
@@ -16,9 +18,13 @@ import { TaxableIncomeCard } from './TaxableIncomeCard'
 const TaxOverviewHeader = () => {
   const { t } = useTranslation()
   const { year } = useTaxEstimatesYear()
+  const { formatDate } = useIntlFormatter()
   const { fullYearProjection } = useFullYearProjection()
   const { isDesktop } = useSizeClass()
   const projectedCondition: 'default' | 'projected' = fullYearProjection ? 'projected' : 'default'
+
+  const formattedYear = formatDate(new Date(year, 0, 1), DateFormat.Year)
+
   const taxableIncomeTitle = tConditional(t, 'taxEstimates:label.taxable_income_for_year', {
     condition: projectedCondition,
     cases: {
@@ -28,7 +34,7 @@ const TaxOverviewHeader = () => {
     contexts: {
       projected: 'projected',
     },
-    year,
+    formattedYear,
   })
 
   const taxableIncomeDescription = tConditional(t, 'taxEstimates:label.taxable_income_estimate_to_date_for_year', {
@@ -40,7 +46,7 @@ const TaxOverviewHeader = () => {
     contexts: {
       projected: 'projected',
     },
-    year,
+    formattedYear,
   })
   return (
     <TaxEstimatesHeader
