6e2cd98de2c4767d2ef9887d67e9fb65d2c47e70
Jim Wei <jim@layerfi.com>
2026-04-21 13:43:26 -0700

    Update TaxCodeSelectDrawer.tsx

--- src/components/TaxCodeSelect/TaxCodeSelectDrawer.tsx
+++ src/components/TaxCodeSelect/TaxCodeSelectDrawer.tsx
@@ -41,9 +41,8 @@ export const TaxCodeSelectDrawer = ({
   if (isClearable) {
     listOptions.unshift({
       id: CLEAR_ROW_ID,
-      label: t('bankTransactions:action.no_tax_code', 'No tax code'),
+      label: t('bankTransactions:action.clear_tax_code', 'Clear tax code'),
       value: null,
-      secondary: true,
     })
   }
 
