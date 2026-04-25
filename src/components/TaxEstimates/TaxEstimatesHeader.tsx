67187f013d418a2e376badf45e4b8b6ef57faac2
Darren <darren.sapalo@gmail.com>
2026-04-24 17:19:50 -0700

    inline props and one conditional

--- src/components/TaxEstimates/TaxEstimatesHeader.tsx
+++ src/components/TaxEstimates/TaxEstimatesHeader.tsx
@@ -7,7 +7,6 @@ import { DateFormat } from '@utils/i18n/date/patterns'
 import { useIntlFormatter } from '@hooks/utils/i18n/useIntlFormatter'
 import { useSizeClass } from '@hooks/utils/size/useWindowSize'
 import { useFullYearProjection, useTaxEstimatesYear } from '@providers/TaxEstimatesRouteStore/TaxEstimatesRouteStoreProvider'
-import { type Spacing } from '@ui/sharedUITypes'
 import { HStack, Stack } from '@ui/Stack/Stack'
 import { ResponsiveDetailHeader } from '@components/ResponsiveDetailView/ResponsiveDetailHeader'
 import { FullYearProjectionComboBox } from '@components/TaxEstimates/FullYearProjectionComboBox/FullYearProjectionComboBox'
@@ -133,25 +132,8 @@ export const TaxEstimatesHeader = ({ type }: TaxEstimatesHeaderProps) => {
   const { isMobile } = useSizeClass()
   const { title, description } = useTaxEstimatesHeaderHook({ type })
 
-  const commonProps = {
-    gap: 'md' as Spacing,
-    justify: 'space-between' as const,
-    align: 'start' as const,
-    fluid: true,
-    pie: 'lg' as Spacing,
-    className: 'Layer__TaxEstimatesHeader',
-    direction: 'row' as const,
-  }
-
-  const dynamicProps = isMobile
-    ? {
-      ...commonProps,
-      direction: 'column' as const,
-    }
-    : commonProps
-
   return (
-    <Stack {...dynamicProps}>
+    <Stack className='Layer__TaxEstimatesHeader' direction={isMobile ? 'column' : 'row'} gap='md' justify='space-between' align='start' fluid pie='lg'>
       <ResponsiveDetailHeader title={title} description={description} />
       <HStack justify='end' className='Layer__TaxEstimatesHeader__ComboBoxContainer'>
         <FullYearProjectionComboBox />
