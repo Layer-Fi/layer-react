8f06a1ab1ca3bb7aff34ee63aeec59e6fc6d90b5
Jim Wei <jim@layerfi.com>
2026-04-27 18:46:52 -0700

    Retrieve tax code in UpsertBTDefaultCategories

--- src/hooks/features/bankTransactions/useUpsertBankTransactionsDefaultCategories.tsx
+++ src/hooks/features/bankTransactions/useUpsertBankTransactionsDefaultCategories.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from 'react'
 
 import { type BankTransaction } from '@internal-types/bankTransactions'
-import { useBankTransactionsCategorizationActions } from '@providers/BankTransactionsCategorizationStore/BankTransactionsCategorizationStoreProvider'
+import { getBankTransactionTaxCodeOption, isExclusionCategory } from '@utils/bankTransactions/shared'
+import { type BankTransactionCategorization, useBankTransactionsCategorizationActions } from '@providers/BankTransactionsCategorizationStore/BankTransactionsCategorizationStoreProvider'
 import { getDefaultSelectedCategoryForBankTransaction } from '@components/BankTransactionCategoryComboBox/utils'
 
 export const useUpsertBankTransactionsDefaultCategories = (bankTransactions: BankTransaction[] | undefined) => {
@@ -10,14 +11,20 @@ export const useUpsertBankTransactionsDefaultCategories = (bankTransactions: Ban
   useEffect(() => {
     if (!bankTransactions) return
 
-    setOnlyNewTransactionCategorizations(new Map(
-      bankTransactions.map(transaction => [
-        transaction.id,
-        {
-          category: getDefaultSelectedCategoryForBankTransaction(transaction),
-          taxCode: null,
-        },
-      ]),
+    setOnlyNewTransactionCategorizations(new Map<string, BankTransactionCategorization>(
+      bankTransactions.map((transaction): [string, BankTransactionCategorization] => {
+        const category = getDefaultSelectedCategoryForBankTransaction(transaction)
+
+        return [
+          transaction.id,
+          {
+            category,
+            taxCode: isExclusionCategory(category)
+              ? null
+              : getBankTransactionTaxCodeOption(transaction, transaction.tax_code ?? null),
+          },
+        ]
+      }),
     ))
   }, [bankTransactions, setOnlyNewTransactionCategorizations])
 }
