c98602cd4f9f6ddc3f98f11f34c149676907c306
Darren <darren.sapalo@gmail.com>
2026-05-01 12:12:55 -0700

    fix: styling

--- src/components/TaxDetails/Operator/Operator.tsx
+++ src/components/TaxDetails/Operator/Operator.tsx
@@ -8,5 +8,5 @@ type OperatorProps = {
 
 export const Operator = ({ sign }: OperatorProps) => {
   if (!sign) return null
-  return <Span className='Layer__TaxDetails__Operator' size='md' variant='subtle' aria-hidden='true'>{sign}</Span>
+  return <Span className='Layer__TaxDetails__Operator' size='lg' variant='subtle' aria-hidden='true'>{sign}</Span>
 }
