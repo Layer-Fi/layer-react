badbf507f6d6ccc166e2b2845bd0a30706938061
Darren <darren.sapalo@gmail.com>
2026-05-06 18:22:34 -0700

    import fix

--- src/hooks/utils/i18n/useGlobalMonthSubtitle.ts
+++ src/hooks/utils/i18n/useGlobalMonthSubtitle.ts
@@ -1,6 +1,6 @@
-import { useGlobalDate } from '@providers/GlobalDateStore/GlobalDateStoreProvider'
 import { MonthYearPattern } from '@utils/i18n/date/patterns'
 import { useIntlFormatter } from '@hooks/utils/i18n/useIntlFormatter'
+import { useGlobalDate } from '@providers/GlobalDateStore/GlobalDateStoreProvider'
 
 export const useGlobalMonthSubtitle = () => {
   const { formatDate } = useIntlFormatter()
