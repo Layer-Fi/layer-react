f530d386d0879d4691260c55e7059c1e306227c5
Darren <darrensapalo@users.noreply.github.com>
2026-05-12 15:21:43 -0700

    Apply suggestions from code review
    Co-authored-by: Sarah Raines <32247313+sarahraines@users.noreply.github.com>

--- src/components/TaxEstimatesSummaryCard/states/TaxEstimatesSummaryCardEmpty.tsx
+++ src/components/TaxEstimatesSummaryCard/states/TaxEstimatesSummaryCardEmpty.tsx
@@ -9,7 +9,7 @@ export function TaxEstimatesSummaryCardEmpty() {
       className='Layer__TaxEstimatesSummaryCard__DataState Layer__data-state--reset'
       status={DataStateStatus.info}
       title={t('taxEstimates:empty.no_tax_estimates_summary', 'Get started with your tax estimates')}
-      description={t('taxEstimates:empty.no_tax_estimates_summary_description', 'Start by importing and categorizing your bank transactions')}
+      description={t('taxEstimates:empty.no_tax_estimates_summary_description', 'Start by importing and categorizing your bank transactions.')}
     />
   )
 }
