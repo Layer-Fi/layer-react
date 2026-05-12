76159e555193de59e37607f996d3bc4c8bd8012b
Darren <darren.sapalo@gmail.com>
2026-05-12 15:13:28 -0700

    combined data state scss classes

--- src/components/TaxEstimatesSummaryCard/states/TaxEstimatesSummaryCardEmpty.tsx
+++ src/components/TaxEstimatesSummaryCard/states/TaxEstimatesSummaryCardEmpty.tsx
@@ -6,7 +6,7 @@ export function TaxEstimatesSummaryCardEmpty() {
   const { t } = useTranslation()
   return (
     <DataState
-      className='Layer__TaxEstimatesSummaryCard__DataState Layer__TaxEstimatesSummaryCard__DataState--empty Layer__data-state--reset'
+      className='Layer__TaxEstimatesSummaryCard__DataState Layer__data-state--reset'
       status={DataStateStatus.info}
       title={t('taxEstimates:empty.no_tax_estimates_summary', 'Get started with your tax estimates')}
       description={t('taxEstimates:empty.no_tax_estimates_summary_description', 'Start by importing and categorizing your bank transactions')}
--- src/components/TaxEstimatesSummaryCard/states/TaxEstimatesSummaryCardError.tsx
+++ src/components/TaxEstimatesSummaryCard/states/TaxEstimatesSummaryCardError.tsx
@@ -6,7 +6,7 @@ export const TaxEstimatesSummaryCardError = () => {
   const { t } = useTranslation()
   return (
     <DataState
-      className='Layer__TaxEstimatesSummaryCard__DataState Layer__TaxEstimatesSummaryCard__DataState--error Layer__data-state--reset'
+      className='Layer__TaxEstimatesSummaryCard__DataState Layer__data-state--reset'
       status={DataStateStatus.failed}
       title={t('taxEstimates:error.load_tax_estimates_summary', 'We couldn\'t load your tax summary')}
       description={t('taxEstimates:error.while_loading_tax_estimates_summary', 'An error occurred while loading your tax summary. Please check your connection and try again.')}
