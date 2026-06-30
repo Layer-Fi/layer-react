4b05fa9a0803abf15f11f99e3bbfea5a4c2a7df3
Sarah Raines <sarah@layerfi.com>
2026-06-30 11:29:57 -0700

    stabilize context value

--- src/contexts/BankAccountsContext/BankAccountsContext.tsx
+++ src/contexts/BankAccountsContext/BankAccountsContext.tsx
@@ -1,19 +1,24 @@
-import { createContext, type PropsWithChildren, useCallback, useContext } from 'react'
+import { createContext, type PropsWithChildren, useCallback, useContext, useMemo } from 'react'
 
 import { type BankAccount } from '@schemas/bankAccounts/bankAccount'
 import { isAnyBankAccountSyncing } from '@utils/bankAccount'
-import {
-  type ListBankAccountsSWRResponse,
-  useListBankAccounts,
-} from '@hooks/api/businesses/[business-id]/bank-accounts/useListBankAccounts'
+import { type ListBankAccountsSWRResponse, useListBankAccounts } from '@hooks/api/businesses/[business-id]/bank-accounts/useListBankAccounts'
 import { usePollingConfig } from '@hooks/utils/swr/usePollingConfig'
 
-type BankAccountsContextValue = Omit<ListBankAccountsSWRResponse, 'swrResponse' | 'error' | 'mutate'>
+type BankAccountsContextValue = Pick<
+  ListBankAccountsSWRResponse,
+  | 'data'
+  | 'disconnectedAccountsRequiringNotification'
+  | 'isError'
+  | 'isLoading'
+  | 'isSyncing'
+  | 'loadingStatus'
+  | 'refetch'
+>
 
 const BankAccountsContext = createContext<BankAccountsContextValue>({
   data: undefined,
   isLoading: false,
-  isValidating: false,
   isError: false,
   refetch: () => Promise.resolve(undefined),
   disconnectedAccountsRequiringNotification: 0,
@@ -35,10 +40,36 @@ function useBankAccountsPollingConfig() {
 
 export function BankAccountsProvider({ children }: PropsWithChildren) {
   const pollingConfig = useBankAccountsPollingConfig()
-  const bankAccounts = useListBankAccounts(pollingConfig)
+  const {
+    data,
+    disconnectedAccountsRequiringNotification,
+    isError,
+    isLoading,
+    isSyncing,
+    loadingStatus,
+    refetch,
+  } = useListBankAccounts(pollingConfig)
+
+  const value = useMemo<BankAccountsContextValue>(() => ({
+    data,
+    disconnectedAccountsRequiringNotification,
+    isError,
+    isLoading,
+    isSyncing,
+    loadingStatus,
+    refetch,
+  }), [
+    data,
+    disconnectedAccountsRequiringNotification,
+    isError,
+    isLoading,
+    isSyncing,
+    loadingStatus,
+    refetch,
+  ])
 
   return (
-    <BankAccountsContext.Provider value={bankAccounts}>
+    <BankAccountsContext.Provider value={value}>
       {children}
     </BankAccountsContext.Provider>
   )
