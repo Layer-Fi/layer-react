24ec225431757ddbc915825f6108368d2fb4babf
Sarah Raines <sarah@layerfi.com>
2026-07-01 11:03:55 -0700

    reorder args

--- src/utils/swr/useSWRInfiniteResult.ts
+++ src/utils/swr/useSWRInfiniteResult.ts
@@ -15,11 +15,6 @@ export function useSWRInfiniteResult<T extends PaginatedResponse<unknown>>(
 ) {
   const { data: pages, setSize } = swrResponse
 
-  const flattenedData = useMemo(
-    () => pages?.flatMap(page => page.data) as FlattenedData<T> | undefined,
-    [pages],
-  )
-
   const pagesRef = useRef(pages)
   pagesRef.current = pages
 
@@ -29,5 +24,10 @@ export function useSWRInfiniteResult<T extends PaginatedResponse<unknown>>(
     }
   }, [setSize])
 
-  return new SWRInfiniteResult(swrResponse, flattenedData, fetchMore)
+  const flattenedData = useMemo(
+    () => pages?.flatMap(page => page.data) as FlattenedData<T> | undefined,
+    [pages],
+  )
+
+  return new SWRInfiniteResult(swrResponse, fetchMore, flattenedData)
 }
