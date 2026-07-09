973c7f1a6ee0f04a3bc4ee8f9a4bb96abd178738
Sarah Raines <sarah@layerfi.com>
2026-07-09 12:38:00 -0700

    add back download button

--- src/components/UnifiedReports/UnifiedReportDetailHeader.tsx
+++ src/components/UnifiedReports/UnifiedReportDetailHeader.tsx
@@ -1,24 +1,36 @@
+import { useSizeClass } from '@hooks/utils/size/useWindowSize'
 import { useDetailUnifiedReport } from '@providers/UnifiedReportStore/UnifiedReportStoreProvider'
 import { BackButton } from '@ui/Button/BackButton'
 import { HStack, VStack } from '@ui/Stack/Stack'
 import { Span } from '@ui/Typography/Text'
 import { UnifiedReportDetailBreadcrumb } from '@components/UnifiedReports/UnifiedReportDetailBreadcrumb'
+import { UnifiedReportDownloadButton } from '@components/UnifiedReports/UnifiedReportDownloadButton'
 
 import './unifiedReportDetailHeader.scss'
 
 export const UnifiedReportDetailHeader = () => {
   const { detailReportConfig, closeDetailReport } = useDetailUnifiedReport()
+  const { isDesktop } = useSizeClass()
 
   if (!detailReportConfig) return null
   const { column } = detailReportConfig
 
   return (
-    <HStack gap='sm' pb='lg' pis='lg' align='center' className='Layer__UnifiedReports__DetailHeader'>
-      <BackButton onPress={closeDetailReport} />
-      <VStack gap='3xs'>
-        <UnifiedReportDetailBreadcrumb />
-        <Span size='sm' variant='subtle'>{column.displayName}</Span>
-      </VStack>
+    <HStack
+      pb='lg'
+      pi='lg'
+      align='center'
+      justify='space-between'
+      className='Layer__UnifiedReports__DetailHeader'
+    >
+      <HStack gap='sm' align='center'>
+        <BackButton onPress={closeDetailReport} />
+        <VStack gap='3xs'>
+          <UnifiedReportDetailBreadcrumb />
+          <Span size='sm' variant='subtle'>{column.displayName}</Span>
+        </VStack>
+      </HStack>
+      <UnifiedReportDownloadButton icon={!isDesktop} />
     </HStack>
   )
 }
