b287d1742e5bf8f9b47846d62993b445ec24dde9
Sarah Raines <sarah@layerfi.com>
2026-07-13 16:19:58 -0700

    feat: remove record menu icons, narrow dropdown, add right caret

--- src/components/BankTransactions/RecordManualTransaction/RecordTransactionMenuButton.tsx
+++ src/components/BankTransactions/RecordManualTransaction/RecordTransactionMenuButton.tsx
@@ -1,9 +1,10 @@
 import { useCallback } from 'react'
-import { HandCoins, Plus, Receipt } from 'lucide-react'
+import { ChevronRight, Plus } from 'lucide-react'
 import { useTranslation } from 'react-i18next'
 
 import { Button } from '@ui/Button/Button'
 import { DropdownMenu, MenuItem, MenuList } from '@ui/DropdownMenu/DropdownMenu'
+import { Spacer } from '@ui/Stack/Stack'
 import { Span } from '@ui/Typography/Text'
 
 function RecordTransactionTrigger({ isDisabled }: { isDisabled?: boolean }) {
@@ -34,16 +35,18 @@ export function RecordTransactionMenuButton({ isDisabled }: RecordTransactionMen
     <DropdownMenu
       ariaLabel={t('bankTransactions:action.record_transaction', 'Record transaction')}
       slots={{ Trigger }}
-      slotProps={{ Dialog: { width: 200 } }}
+      slotProps={{ Dialog: { width: 180 } }}
     >
       <MenuList>
         <MenuItem>
-          <Receipt size={16} />
           <Span size='sm'>{t('bankTransactions:action.record_expense', 'Record expense')}</Span>
+          <Spacer />
+          <ChevronRight size={12} />
         </MenuItem>
         <MenuItem>
-          <HandCoins size={16} />
           <Span size='sm'>{t('bankTransactions:action.record_income', 'Record income')}</Span>
+          <Spacer />
+          <ChevronRight size={12} />
         </MenuItem>
       </MenuList>
     </DropdownMenu>
